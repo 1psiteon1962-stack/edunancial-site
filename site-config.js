e2ff459f820524f71763d4a85531ce09c9d021ce
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-08-31 18:11:58 -0700

    Create site-config.js

---
+++ site-config.js
@@ -0,0 +1,9 @@
+<script>
+// Edunancial global config
+window.EDN_CONFIG = {
+  PRELAUNCH: true,             // <-- set to false when Square is ready
+  GO_LIVE_NOTE: "Payments go live this week after the bank holiday.",
+  CONTACT_EMAIL: "hello@edunancial.com", // optional, shows in the banner
+  FORM_NAME: "edn-waitlist"    // Netlify form name
+};
+</script>
