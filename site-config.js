72a14d36e20cc312ac3fbd400916ada2aaac3f11
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-09-06 17:35:07 -0700

    Delete site-config.js

--- site-config.js
+++
@@ -1,9 +0,0 @@
-<script>
-// Edunancial global config
-window.EDN_CONFIG = {
-  PRELAUNCH: true,             // <-- set to false when Square is ready
-  GO_LIVE_NOTE: "Payments go live this week after the bank holiday.",
-  CONTACT_EMAIL: "hello@edunancial.com", // optional, shows in the banner
-  FORM_NAME: "edn-waitlist"    // Netlify form name
-};
-</script>
