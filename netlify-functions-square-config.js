16675a0265f4f4824bf60f336e8bab1852fce036
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-09-10 09:10:55 -0700

    Delete netlify-functions-square-config.js

--- netlify-functions-square-config.js
+++
@@ -1,8 +0,0 @@
-export const handler = async () => {
-  const appId = process.env.SQUARE_APP_ID;
-  const locationId = process.env.SQUARE_LOCATION_ID;
-  if (!appId || !locationId) {
-    return { statusCode: 500, body: JSON.stringify({ error: "Missing Square config" }) };
-  }
-  return { statusCode: 200, headers: { "Content-Type": "application/json" }, body: JSON.stringify({ appId, locationId }) };
-};
