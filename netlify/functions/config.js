c31466c14bcb52f9027a1b22edbe951e4a9152e6
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-09-10 20:53:55 -0700

    Update config.js

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,2 +1,10 @@
-
-add config.js Netlify function
+// netlify/functions/config.js
+exports.handler = async function () {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      applicationId: process.env.SQUARE_APPLICATION_ID,
+      locationId: process.env.SQUARE_LOCATION_ID,
+    }),
+  };
+};
