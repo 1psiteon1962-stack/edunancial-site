c47b0b60ace6f8e2fcbc1a043f8d6609f2239624
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-05 11:50:04 -0800

    Refactor page layout and section imports
    Updated section imports to use lowercase file names and adjusted the layout for better structure.

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,56 @@
-// app/page.tsx
+"use client";
 
-import HeroSection from "../components/sections/HeroSection";
-import StorySection from "../components/sections/StorySection";
-import AppsSection from "../components/sections/AppsSection";
-import CoursesSection from "../components/sections/CoursesSection";
-import RotatingVideoSection from "../components/sections/RotatingVideoSection";
-import BooksSection from "../components/sections/BooksSection";
-import FooterSection from "../components/sections/FooterSection";
+import React from "react";
 
+// ===== IMPORT SECTIONS =====
+import HeroSection from "@/components/sections/herosection";
+import StorySection from "@/components/sections/storysection";
+import BooksSection from "@/components/sections/bookssection";
+import CoursesSection from "@/components/sections/coursessection";
+import AppsSection from "@/components/sections/appssection";
+import RotatingVideoSection from "@/components/sections/rotatingvideosection";
+import FooterSection from "@/components/sections/footersection";
+
+// ===== PAGE CONTAINER =====
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col bg-slate-50">
-      <HeroSection />
-      <StorySection />
-      <AppsSection />
-      <CoursesSection />
-      <RotatingVideoSection />
-      <BooksSection />
-      <FooterSection />
+    <main className="flex flex-col items-center justify-center w-full overflow-x-hidden bg-white text-black">
+
+      {/* HERO SECTION */}
+      <section className="w-full">
+        <HeroSection />
+      </section>
+
+      {/* STORY SECTION */}
+      <section className="w-full">
+        <StorySection />
+      </section>
+
+      {/* BOOKS SECTION */}
+      <section className="w-full">
+        <BooksSection />
+      </section>
+
+      {/* COURSES SECTION */}
+      <section className="w-full">
+        <CoursesSection />
+      </section>
+
+      {/* APPS SECTION */}
+      <section className="w-full">
+        <AppsSection />
+      </section>
+
+      {/* ROTATING VIDEO SECTION */}
+      <section className="w-full">
+        <RotatingVideoSection />
+      </section>
+
+      {/* FOOTER */}
+      <section className="w-full">
+        <FooterSection />
+      </section>
+
     </main>
   );
 }
