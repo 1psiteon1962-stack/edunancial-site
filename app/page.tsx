60f09152b1f1ba47bd18256e354184329ea35561
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-11 19:24:01 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,21 @@
+"use client";
+
 import HeroSection from "@/components/sections/herosection";
+import RotatingVideoSection from "@/components/sections/rotatingvideosection";
 import StorySection from "@/components/sections/storysection";
 import BooksSection from "@/components/sections/bookssection";
 import CoursesSection from "@/components/sections/coursessection";
 import AppsSection from "@/components/sections/appssection";
-import RotatingVideoSection from "@/components/sections/rotatingvideosection";
 
-export default function HomePage() {
+export default function Home() {
   return (
-    <>
+    <div className="flex flex-col gap-16">
       <HeroSection />
+      <RotatingVideoSection />
       <StorySection />
       <BooksSection />
       <CoursesSection />
       <AppsSection />
-      <RotatingVideoSection />
-    </>
+    </div>
   );
 }
