b521f5a32f51f06c876763ad3610bf2107b6221a
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-11 19:08:36 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,19 @@
-"use client";
-
-import React from "react";
-
-// ===== IMPORT SECTIONS =====
 import HeroSection from "@/components/sections/herosection";
 import StorySection from "@/components/sections/storysection";
 import BooksSection from "@/components/sections/bookssection";
 import CoursesSection from "@/components/sections/coursessection";
 import AppsSection from "@/components/sections/appssection";
 import RotatingVideoSection from "@/components/sections/rotatingvideosection";
-import FooterSection from "@/components/sections/footersection";
 
-// ===== PAGE CONTAINER =====
 export default function HomePage() {
   return (
-    <main className="flex flex-col items-center justify-center w-full overflow-x-hidden bg-white text-black">
-
-      {/* HERO SECTION */}
-      <section className="w-full">
-        <HeroSection />
-      </section>
-
-      {/* STORY SECTION */}
-      <section className="w-full">
-        <StorySection />
-      </section>
-
-      {/* BOOKS SECTION */}
-      <section className="w-full">
-        <BooksSection />
-      </section>
-
-      {/* COURSES SECTION */}
-      <section className="w-full">
-        <CoursesSection />
-      </section>
-
-      {/* APPS SECTION */}
-      <section className="w-full">
-        <AppsSection />
-      </section>
-
-      {/* ROTATING VIDEO SECTION */}
-      <section className="w-full">
-        <RotatingVideoSection />
-      </section>
-
-      {/* FOOTER */}
-      <section className="w-full">
-        <FooterSection />
-      </section>
-
-    </main>
+    <>
+      <HeroSection />
+      <StorySection />
+      <BooksSection />
+      <CoursesSection />
+      <AppsSection />
+      <RotatingVideoSection />
+    </>
   );
 }
