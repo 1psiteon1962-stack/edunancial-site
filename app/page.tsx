8bda3cf82583690219704c78dbe2dbdfbd4fd410
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-11 20:09:49 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,40 @@
-"use client";
-
 import HeroSection from "@/components/sections/herosection";
 import RotatingVideoSection from "@/components/sections/rotatingvideosection";
 import StorySection from "@/components/sections/storysection";
 import BooksSection from "@/components/sections/bookssection";
 import CoursesSection from "@/components/sections/coursessection";
 import AppsSection from "@/components/sections/appssection";
+import FooterSection from "@/components/sections/footersection";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="flex flex-col gap-16">
+    <main className="w-full overflow-x-hidden">
+      {/* HERO */}
       <HeroSection />
+
+      {/* VIDEO / VISUAL INTRO */}
       <RotatingVideoSection />
+
+      {/* STORY / MISSION */}
       <StorySection />
-      <BooksSection />
-      <CoursesSection />
-      <AppsSection />
-    </div>
+
+      {/* BOOKS */}
+      <section id="books">
+        <BooksSection />
+      </section>
+
+      {/* COURSES */}
+      <section id="courses">
+        <CoursesSection />
+      </section>
+
+      {/* APPS */}
+      <section id="apps">
+        <AppsSection />
+      </section>
+
+      {/* FOOTER */}
+      <FooterSection />
+    </main>
   );
 }
