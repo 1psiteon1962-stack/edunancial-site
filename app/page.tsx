636a0d43496bf8c44d455eb3c00d41d6a7d19c79
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-11 20:21:28 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,39 @@
 import HeroSection from "@/components/sections/herosection";
 import RotatingVideoSection from "@/components/sections/rotatingvideosection";
-import StorySection from "@/components/sections/storysection";
 import BooksSection from "@/components/sections/bookssection";
 import CoursesSection from "@/components/sections/coursessection";
 import AppsSection from "@/components/sections/appssection";
+import StorySection from "@/components/sections/storysection";
 import FooterSection from "@/components/sections/footersection";
 
 export default function HomePage() {
   return (
-    <main className="w-full overflow-x-hidden">
-      {/* HERO */}
-      <HeroSection />
+    <>
+      <section id="hero">
+        <HeroSection />
+      </section>
 
-      {/* VIDEO / VISUAL INTRO */}
-      <RotatingVideoSection />
+      <section id="videos">
+        <RotatingVideoSection />
+      </section>
 
-      {/* STORY / MISSION */}
-      <StorySection />
-
-      {/* BOOKS */}
       <section id="books">
         <BooksSection />
       </section>
 
-      {/* COURSES */}
       <section id="courses">
         <CoursesSection />
       </section>
 
-      {/* APPS */}
       <section id="apps">
         <AppsSection />
       </section>
 
-      {/* FOOTER */}
+      <section id="story">
+        <StorySection />
+      </section>
+
       <FooterSection />
-    </main>
+    </>
   );
 }
