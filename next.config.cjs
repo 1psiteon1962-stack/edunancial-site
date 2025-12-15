8a32b12f40d879a22ab3f977bd9f3c3fa486d1e5
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 10:05:31 -0800

    Update next.config.cjs

--- next.config.cjs
+++ next.config.cjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone'
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
