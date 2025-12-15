97df8fa0e23aad4bf56af7748e8af490c74f8a68
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 10:51:47 -0800

    Delete components/sections/bookssection.tsx

--- components/sections/bookssection.tsx
+++
@@ -1,8 +0,0 @@
-export default function BooksSection() {
-  return (
-    <section id="books">
-      <h2>Books</h2>
-      <p>Edunancial books and publications will appear here.</p>
-    </section>
-  );
-}
