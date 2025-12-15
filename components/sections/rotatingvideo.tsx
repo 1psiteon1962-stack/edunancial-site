f028bb6d05324be08a759497c44c590ed3238b2f
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 14:54:28 -0800

    Delete components/sections/rotatingvideo.tsx

--- components/sections/rotatingvideo.tsx
+++
@@ -1,8 +0,0 @@
-export default function RotatingVideoSection() {
-  return (
-    <section id="videos">
-      <h2>Featured Videos</h2>
-      <p>Rotating educational content goes here.</p>
-    </section>
-  );
-}
