c1ae77ffef52abfcd602bc1b00f91253824ecf76
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 13:47:55 -0800

    Update index.tsx

--- components/sections/index.tsx
+++ components/sections/index.tsx
@@ -4,7 +4,5 @@ export { default as BooksSection } from "./BooksSection";
 export { default as CoursesSection } from "./CoursesSection";
 export { default as FooterSection } from "./FooterSection";
 
-// these two ARE lowercase in your repo (per your screenshot)
 export { default as RotatingVideoSection } from "./rotatingvideosection";
 export { default as StorySection } from "./storysection";
-```0
