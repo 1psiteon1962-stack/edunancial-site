3825ef85774303fdb5ea28e3c103ab8c592aa67f
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 15:27:17 -0800

    Update footersection.tsx

--- components/sections/footersection.tsx
+++ components/sections/footersection.tsx
@@ -1,7 +1,7 @@
 export default function FooterSection() {
   return (
     <footer>
-      <p>© {new Date().getFullYear()} Edunancial</p>
+      <p>© {new Date().getFullYear()} Edunancial. All rights reserved.</p>
     </footer>
   );
 }
