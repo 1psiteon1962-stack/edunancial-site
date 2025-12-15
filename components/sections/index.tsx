af942afc04e929d6ef19ad25793d02bd199b2cdb
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 13:00:47 -0800

    Update index.tsx

--- components/sections/index.tsx
+++ components/sections/index.tsx
@@ -1,21 +1,11 @@
-import React from "react";
+import RotatingVideoSection from "./rotatingvideosection";
+import StorySection from "./storysection";
 
-export function HeroSection() {
-  return <div>HERO OK</div>;
-}
-
-export function AppsSection() {
-  return <div>APPS OK</div>;
-}
-
-export function BooksSection() {
-  return <div>BOOKS OK</div>;
-}
-
-export function CoursesSection() {
-  return <div>COURSES OK</div>;
-}
-
-export function FooterSection() {
-  return <div>FOOTER OK</div>;
+export default function Sections() {
+  return (
+    <>
+      <RotatingVideoSection />
+      <StorySection />
+    </>
+  );
 }
