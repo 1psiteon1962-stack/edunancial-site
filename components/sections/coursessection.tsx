faa99e962d7b2227afdfe9101af71e3175de7bd1
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 14:48:09 -0800

    Create coursessection.tsx

---
+++ components/sections/coursessection.tsx
@@ -0,0 +1,8 @@
+export default function CoursesSection() {
+  return (
+    <section id="courses">
+      <h2>Courses</h2>
+      <p>Educational courses and learning material.</p>
+    </section>
+  );
+}
