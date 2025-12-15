737273db5a905a79d2805bb91def803cc20c8ff8
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 10:52:05 -0800

    Delete components/sections/coursessection.tsx

--- components/sections/coursessection.tsx
+++
@@ -1,8 +0,0 @@
-export default function CoursesSection() {
-  return (
-    <section id="courses">
-      <h2>Courses</h2>
-      <p>Edunancial courses and training programs will appear here.</p>
-    </section>
-  );
-}
