d341c9e8794d9019d8899a4561fe721291b25855
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 10:35:45 -0800

    Update page.tsx

--- app/en/page.tsx
+++ app/en/page.tsx
@@ -1,9 +1,7 @@
-// FILE: app/en/page.tsx
-
 import HeroSection from "@/components/sections/herosection";
 import AppsSection from "@/components/sections/appssection";
 import BooksSection from "@/components/sections/bookssection";
-import CoursesSection from "@/components/sections/coursessecti on";
+import CoursesSection from "@/components/sections/coursessection";
 import FooterSection from "@/components/sections/footersection";
 
 export default function Page() {
