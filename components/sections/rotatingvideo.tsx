6f8bb2bf5ea1d1691e98c567c53f1779eeffe71e
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 14:51:00 -0800

    Create rotatingvideo.tsx

---
+++ components/sections/rotatingvideo.tsx
@@ -0,0 +1,8 @@
+export default function RotatingVideoSection() {
+  return (
+    <section id="videos">
+      <h2>Featured Videos</h2>
+      <p>Rotating educational content goes here.</p>
+    </section>
+  );
+}
