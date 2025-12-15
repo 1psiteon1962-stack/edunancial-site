32244e68c715914eb055dac6b14dc3b7cef302a9
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 10:42:10 -0800

    Create index.ts

---
+++ components/sections/index.ts
@@ -0,0 +1,5 @@
+export { default as HeroSection } from "./herosection";
+export { default as AppsSection } from "./appssection";
+export { default as BooksSection } from "./bookssection";
+export { default as CoursesSection } from "./coursessection";
+export { default as FooterSection } from "./footersection";
