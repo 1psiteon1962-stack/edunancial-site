6068b815111680211e29b29be8a38fc719e84e2a
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 15:12:42 -0800

    Update layout.tsx

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -2,7 +2,27 @@ import {
   HeroSection,
   AppsSection,
   CoursesSection,
-  RotatingVideoSection,
   StorySection,
+  RotatingVideoSection,
   FooterSection,
 } from "@/components/sections";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <HeroSection />
+        <AppsSection />
+        <CoursesSection />
+        <StorySection />
+        <RotatingVideoSection />
+        <FooterSection />
+        {children}
+      </body>
+    </html>
+  );
+}
