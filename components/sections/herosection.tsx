cdf1f81b12e8601677ef867d9d170bcdaaacda4e
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 10:52:21 -0800

    Delete components/sections/herosection.tsx

--- components/sections/herosection.tsx
+++
@@ -1,8 +0,0 @@
-export default function HeroSection() {
-  return (
-    <section id="hero">
-      <h1>Edunancial</h1>
-      <p>Financial education built for global scale.</p>
-    </section>
-  );
-}
