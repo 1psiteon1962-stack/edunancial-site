d3bee92be13a65d90c29b45a5b865bcff259a745
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 12:36:52 -0800

    Update and rename index.ts to index.tsx

--- components/sections/index.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const HeroSection = () => <div>HERO OK</div>;
-export const AppsSection = () => <div>APPS OK</div>;
-export const BooksSection = () => <div>BOOKS OK</div>;
-export const CoursesSection = () => <div>COURSES OK</div>;
-export const FooterSection = () => <div>FOOTER OK</div>;
---
+++ components/sections/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export function HeroSection() {
+  return <div>HERO OK</div>;
+}
+
+export function AppsSection() {
+  return <div>APPS OK</div>;
+}
+
+export function BooksSection() {
+  return <div>BOOKS OK</div>;
+}
+
+export function CoursesSection() {
+  return <div>COURSES OK</div>;
+}
+
+export function FooterSection() {
+  return <div>FOOTER OK</div>;
+}
