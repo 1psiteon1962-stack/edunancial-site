28e7f6cda174d85620f03ee233c73b825ac570e3
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 21:08:15 -0800

    Update storysection.tsx

--- components/sections/storysection.tsx
+++ components/sections/storysection.tsx
@@ -1,7 +1,8 @@
-export default function storysection() {
+export default function StorySection() {
   return (
     <section id="story">
-      <h2>story</h2>
+      <h2>our story</h2>
+      <p>education before capital. structure before scale.</p>
     </section>
   );
 }
