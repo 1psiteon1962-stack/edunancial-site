d429a5d7dd09207dcd02a0363604b4a4373b6e9c
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-16 15:36:07 -0800

    Update footersection.tsx

--- components/sections/footersection.tsx
+++ components/sections/footersection.tsx
@@ -1,9 +1,8 @@
-"use client";
-
-export default function FooterSection() {
+export default function footersection({ copy }: any) {
   return (
-    <footer>
-      <p>Footer content</p>
+    <footer style={{ padding: "2rem 1.5rem", maxWidth: 960, margin: "0 auto" }}>
+      <hr />
+      <small>{copy.footerNote}</small>
     </footer>
   );
 }
