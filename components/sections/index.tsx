452969514ebc2d240c1fbd2f21b91ddaf70794e1
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 19:17:18 -0800

    Update and rename Index.tsx to index.tsx

---
+++ components/sections/index.tsx
@@ -0,0 +1,27 @@
+import herosection from './herosection';
+import appssection from './appssection';
+import booksection from './booksection';
+import coursessection from './coursessection';
+import rotatingvideosection from './rotatingvideosection';
+import storysection from './storysection';
+import footersection from './footersection';
+
+export {
+  herosection,
+  appssection,
+  booksection,
+  coursessection,
+  rotatingvideosection,
+  storysection,
+  footersection,
+};
+
+export default {
+  herosection,
+  appssection,
+  booksection,
+  coursessection,
+  rotatingvideosection,
+  storysection,
+  footersection,
+};
