a9b93a099daec904780a1db81a20f1a9be0ea284
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 19:25:34 -0800

    Update coursessection.tsx

--- components/sections/coursessection.tsx
+++ components/sections/coursessection.tsx
@@ -2,7 +2,6 @@ export default function coursessection() {
   return (
     <section id="courses">
       <h2>courses</h2>
-      <p>progressive education for entrepreneurs</p>
     </section>
   );
 }
