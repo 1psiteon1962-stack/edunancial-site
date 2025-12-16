71fe497bed839c703d29dd17bf027dc02fe0eabd
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 18:53:32 -0800

    Update Index.tsx

--- components/sections/Index.tsx
+++ components/sections/Index.tsx
@@ -1,7 +1,29 @@
-export { HeroSection } from './HeroSection';
-export { AppsSection } from './AppsSection';
-export { BooksSection } from './BooksSection';
-export { CoursesSection } from './CoursesSection';
-export { RotatingVideoSection } from './RotatingVideoSection';
-export { StorySection } from './StorySection';
-export { FooterSection } from './FooterSection';
+import HeroSection from './HeroSection';
+import AppsSection from './AppsSection';
+import BookSection from './BookSection';
+import CoursesSection from './CoursesSection';
+import RotatingVideoSection from './RotatingVideoSection';
+import StorySection from './StorySection';
+import FooterSection from './FooterSection';
+
+export {
+  HeroSection,
+  AppsSection,
+  BookSection,
+  CoursesSection,
+  RotatingVideoSection,
+  StorySection,
+  FooterSection,
+};
+
+const Sections = {
+  HeroSection,
+  AppsSection,
+  BookSection,
+  CoursesSection,
+  RotatingVideoSection,
+  StorySection,
+  FooterSection,
+};
+
+export default Sections;
