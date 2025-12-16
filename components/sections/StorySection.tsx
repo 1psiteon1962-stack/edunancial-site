b3f301686bae8fd109edc0a2564741f24f7dfb40
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 19:13:53 -0800

    Update and rename StorySection.tsx to storysection.tsx

--- components/sections/StorySection.tsx
+++
@@ -1,8 +0,0 @@
-export function StorySection() {
-  return (
-    <section id="story">
-      <h2>Our Story</h2>
-      <p>Why Edunancial exists.</p>
-    </section>
-  );
-}
---
+++ components/sections/storysection.tsx
@@ -0,0 +1,8 @@
+export default function storysection() {
+  return (
+    <section id="story">
+      <h2>story</h2>
+      <p>education built from experience</p>
+    </section>
+  );
+}
