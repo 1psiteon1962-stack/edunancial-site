10eb78d9e1e14df4a879e868375f2c173ac2c60b
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 21:56:32 -0800

    Update index.tsx

--- components/sections/index.tsx
+++ components/sections/index.tsx
@@ -1,6 +1,6 @@
-export { default as AppSection } from './appsection';
+export { default as AppsSection } from './appssection';
 export { default as BookSection } from './booksection';
-export { default as CoursesSection } from './coursessecction';
+export { default as CoursesSection } from './coursessection';
 export { default as HeroSection } from './herosection';
 export { default as RotatingVideoSection } from './rotatingvideosection';
 export { default as StorySection } from './storysection';
