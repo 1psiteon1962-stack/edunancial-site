b35b04dbbc169d435c3cedadbcee2219bc7284b5
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 19:12:10 -0800

    Update and rename RotatingVideoSection.tsx to rotatingvideosection.tsx

--- components/sections/RotatingVideoSection.tsx
+++
@@ -1,8 +0,0 @@
-export function RotatingVideoSection() {
-  return (
-    <section id="videos">
-      <h2>Video Insights</h2>
-      <p>Short-form insights that compound over time.</p>
-    </section>
-  );
-}
---
+++ components/sections/rotatingvideosection.tsx
@@ -0,0 +1,8 @@
+export default function rotatingvideosection() {
+  return (
+    <section id="videos">
+      <h2>video</h2>
+      <p>strategy in motion</p>
+    </section>
+  );
+}
