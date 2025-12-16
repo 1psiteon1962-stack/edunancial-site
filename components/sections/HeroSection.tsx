4082a1f84ffcdddb9bbb75c4da1ee779a6d0898a
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 19:04:37 -0800

    Update and rename HeroSection.tsx to herosection.tsx

--- components/sections/HeroSection.tsx
+++
@@ -1,8 +0,0 @@
-export function HeroSection() {
-  return (
-    <section id="hero">
-      <h1>Edunancial</h1>
-      <p>Financial literacy for the next generation.</p>
-    </section>
-  );
-}
---
+++ components/sections/herosection.tsx
@@ -0,0 +1,8 @@
+export default function herosection() {
+  return (
+    <section id="hero">
+      <h1>edunancial</h1>
+      <p>build the entrepreneur you were meant to become</p>
+    </section>
+  );
+}
