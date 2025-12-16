1ace3b3dbe65839b1f510b5f8a6b1d63edc58be4
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-15 21:46:30 -0800

    Update index.tsx

--- components/sections/index.tsx
+++ components/sections/index.tsx
@@ -1,3 +1,7 @@
-git add components/sections/index.tsx
-git commit -m "Fix section imports to match filenames"
-git push
+export { default as AppSection } from './appsection';
+export { default as BookSection } from './booksection';
+export { default as CoursesSection } from './coursessecction';
+export { default as HeroSection } from './herosection';
+export { default as RotatingVideoSection } from './rotatingvideosection';
+export { default as StorySection } from './storysection';
+export { default as FooterSection } from './footersection';
