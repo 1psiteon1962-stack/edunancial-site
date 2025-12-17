42200f08fd3bc7e043be80e29722ae973271afbf
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-16 20:04:35 -0800

    Update booksection.tsx

--- components/sections/booksection.tsx
+++ components/sections/booksection.tsx
@@ -1,8 +1,1 @@
-export default function BooksSection({ copy }: any) {
-  return (
-    <section>
-      <h3>{copy?.booksTitle}</h3>
-      <p>{copy?.booksBody}</p>
-    </section>
-  );
-}
+export { default } from './BooksSection';
