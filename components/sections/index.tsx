d9e8e188a1fdf2bd68dfe367388a619b00854ce4
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-18 06:32:16 -0800

    Update index.tsx

--- components/sections/index.tsx
+++ components/sections/index.tsx
@@ -1,7 +1,14 @@
 export { default as HeroSection } from './HeroSection';
 export { default as StorySection } from './StorySection';
-export { default as AppsSection } from './AppsSection';
 export { default as CoursesSection } from './CoursesSection';
-export { default as BooksSection } from './BooksSection';
+export { default as AppsSection } from './AppsSection';
 export { default as RotatingVideoSection } from './RotatingVideoSection';
 export { default as FooterSection } from './FooterSection';
+
+/**
+ * IMPORTANT:
+ * We export BOTH names to satisfy existing imports.
+ * This avoids touching app/* pages.
+ */
+export { default as BooksSection } from './BooksSection';
+export { default as BookSection } from './BooksSection';
