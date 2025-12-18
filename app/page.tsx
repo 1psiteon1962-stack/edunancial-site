24dcaa30d0de229ad4fae8a94a5e26d3a0f786d7
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-18 14:48:13 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import SiteHome from '../../components/sitehome';
+import SiteHome from "../../components/sitehome";
 
-export default function HomePage() {
+export default function Page() {
   return <SiteHome />;
 }
