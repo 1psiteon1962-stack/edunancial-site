bbe25d04eacdfb7711f6660b791e6f1fc3d8f81a
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-19 10:21:42 -0800

    Create global.ts

---
+++ data/content/global.ts
@@ -0,0 +1,18 @@
+const globalContent = {
+  region: "GLOBAL",
+  currency: "MULTI",
+  language: "multi",
+  hero: {
+    title: "Global Financial Literacy",
+    subtitle:
+      "One platform. Multiple regions. Education without borders.",
+  },
+  availability: {
+    courses: true,
+    books: true,
+    apps: true,
+    payments: false,
+  },
+};
+
+export default globalContent;
