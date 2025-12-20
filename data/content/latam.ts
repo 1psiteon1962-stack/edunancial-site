80b04e148ff5ca51c9006fbab367da09ffb0c6b6
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-20 11:03:09 -0800

    Create latam.ts

---
+++ data/content/latam.ts
@@ -0,0 +1,14 @@
+const latamContent = {
+  region: "latam",
+  heroTitle: "Educación Financiera Para Crecer",
+  heroSubtitle:
+    "Estructura, capital y claridad financiera para emprendedores y familias.",
+  focus: [
+    "Negocios",
+    "Inversión",
+    "Familia",
+    "Juventud",
+  ],
+};
+
+export default latamContent;
