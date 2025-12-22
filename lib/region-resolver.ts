48cc6c103085ea3de33098eb923fbe87a03401a4
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-21 17:41:35 -0800

    Update region-resolver.ts

--- lib/region-resolver.ts
+++ lib/region-resolver.ts
@@ -1,9 +1,5 @@
-// lib/region-resolver.ts
-
-import type { Region } from "./regions";
+import { Region } from "./region";
 
 export function resolveRegion(): Region {
-  // You can improve this later (geo IP, headers, etc.)
-  // For now this is deterministic and type-safe
   return "NA";
 }
