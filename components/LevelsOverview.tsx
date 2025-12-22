f4d3e5b61039f9af1f87022b0c3cfbf9a18b3ce2
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-22 10:10:44 -0800

    Update LevelsOverview.tsx

--- components/LevelsOverview.tsx
+++ components/LevelsOverview.tsx
@@ -1,27 +1,19 @@
 // components/LevelsOverview.tsx
 
-import { EDUNANCIAL_LEVELS } from "@/lib/levels"
+import { EDUNANCIAL_LEVELS } from "@/lib/levels";
 
 export default function LevelsOverview() {
   return (
     <section>
-      <h2>Edunancial Levels</h2>
-
       <ul>
         {EDUNANCIAL_LEVELS.map((lvl) => (
           <li key={lvl.id}>
-            <strong>{lvl.label}</strong>
+            <strong>{lvl.title}</strong>
             <br />
             {lvl.description}
-            <br />
-            <strong>Audience:</strong> {lvl.audience}
-            <br />
-            <strong>Access:</strong> {lvl.access}
-            <br />
-            <strong>Monetization:</strong> {lvl.monetization}
           </li>
         ))}
       </ul>
     </section>
-  )
+  );
 }
