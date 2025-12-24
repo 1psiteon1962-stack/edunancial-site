9efcda77094598bffad4ef1ccb5ea13489b97116
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-23 20:26:40 -0800

    Create not-found.tsx

---
+++ app/africa/not-found.tsx
@@ -0,0 +1,10 @@
+export default function AfricaNotFound() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Page Not Found</h1>
+      <p>
+        This page does not exist on the Edunancial Africa site.
+      </p>
+    </main>
+  );
+}
