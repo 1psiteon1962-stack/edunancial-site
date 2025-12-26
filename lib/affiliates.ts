1d9435feb012139f66f47b7c24c482dc8521b609
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-26 09:52:50 -0800

    Update affiliates.ts

--- lib/affiliates.ts
+++ lib/affiliates.ts
@@ -1,16 +1,9 @@
-export type AffiliateEvent = {
-  affiliateId: string;
-  offerId: string;
-  amount: number;
+export type Affiliate = {
+  id: string;
+  name: string;
 };
 
-const AFFILIATE_LOG: AffiliateEvent[] = [];
-
-export function recordAffiliateSale(event: AffiliateEvent) {
-  AFFILIATE_LOG.push(event);
-}
-
-export function getAffiliateEarnings(id: string) {
-  return AFFILIATE_LOG.filter(a => a.affiliateId === id)
-    .reduce((sum, a) => sum + a.amount * 0.2, 0); // 20% commission
-}
+export const AFFILIATES: Affiliate[] = [
+  { id: "default", name: "Default Partner" },
+  { id: "founder", name: "Founder Channel" },
+];
