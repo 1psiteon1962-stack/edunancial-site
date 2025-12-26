9d2430be2cead75d1d8f596521025b26249b2b46
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-26 09:33:09 -0800

    Create affiliateRegistry.ts

---
+++ lib/affiliateRegistry.ts
@@ -0,0 +1,11 @@
+export type Affiliate = {
+  id: string;
+  name: string;
+};
+
+export const AFFILIATES: Affiliate[] = [
+  { id: "global-partner-1", name: "Global Partner 1" },
+  { id: "latam-network", name: "LATAM Network" },
+  { id: "africa-hub", name: "Africa Hub" },
+  { id: "institutional-sponsor", name: "Institutional Sponsor" },
+];
