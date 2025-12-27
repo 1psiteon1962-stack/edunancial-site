1f188ad8df894aea07b55aae5c973f973d131389
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-26 20:19:54 -0800

    Refactor SuccessPage to use providers

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,22 +1,2 @@
-'use client';
-
-import { useEffect } from 'react';
-import { useCart } from '@/app/providers/CartProvider';
-import { useEnrollment } from '@/app/providers/EnrollmentProvider';
-
-export default function SuccessPage() {
-  const { items, clearCart } = useCart();
-  const { enroll } = useEnrollment();
-
-  useEffect(() => {
-    items.forEach(item => enroll(item.id));
-    clearCart();
-  }, [items, enroll, clearCart]);
-
-  return (
-    <main>
-      <h1>Payment Successful</h1>
-      <p>You are now enrolled in your courses.</p>
-    </main>
-  );
-}
+import { CartProvider } from "../providers/CartProvider";
+import { EnrollmentProvider } from "../providers/EnrollmentProvider";
