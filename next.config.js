787aefcd49e7b65a57055b40758024a57f9f1dae
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-28 13:35:15 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const path = require("path");
-
 const nextConfig = {
   reactStrictMode: true,
-
-  output: "export",
-  trailingSlash: true,
-
-  images: {
-    unoptimized: true,
-  },
-
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname);
-    return config;
-  },
 };
 
 module.exports = nextConfig;
