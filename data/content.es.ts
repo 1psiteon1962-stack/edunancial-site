2197715ac0d505f6d0661765d2d7f4a70b084206
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-31 11:33:03 -0800

    Create content.es.ts

---
+++ data/content.es.ts
@@ -0,0 +1,7 @@
+// data/content.es.ts
+
+export const content = {
+  doctrineTitle: "La Doctrina Edunancial",
+  doctrineBody:
+    "El capitalismo se consolida. Edunancial prepara a las personas antes de que pierdan su ventaja estructural.",
+};
