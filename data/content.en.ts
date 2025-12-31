136f478ad7562583d702372a0700cb2ae56e43db
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-31 11:31:26 -0800

    Create content.en.ts

---
+++ data/content.en.ts
@@ -0,0 +1,7 @@
+// data/content.en.ts
+
+export const content = {
+  doctrineTitle: "The Edunancial Doctrine",
+  doctrineBody:
+    "Capitalism consolidates. Edunancial prepares individuals before leverage is lost.",
+};
