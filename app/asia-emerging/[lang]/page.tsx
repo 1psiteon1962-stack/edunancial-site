441c3c914a7b9ece3e6ddc9029ccfb0bf8a8de5f
1psiteon1962-stack <1psiteon1962@gmail.com>
2025-12-31 17:50:38 -0800

    Update page.tsx

--- app/asia-emerging/[lang]/page.tsx
+++ app/asia-emerging/[lang]/page.tsx
@@ -1,7 +1,13 @@
 // app/asia-emerging/[lang]/page.tsx
+
+import { REGION_LANGUAGES, Language } from "@/lib/i18n";
 import LocalizedDoctrine from "@/components/LocalizedDoctrine";
 import CurriculumPath from "@/components/CurriculumPath";
-import { Language } from "@/lib/i18n";
+import CapitalismAssessment from "@/components/CapitalismAssessment";
+
+export function generateStaticParams() {
+  return REGION_LANGUAGES["asia-emerging"].map((lang) => ({ lang }));
+}
 
 export default function AsiaEmergingPage({
   params,
@@ -12,6 +18,7 @@ export default function AsiaEmergingPage({
     <>
       <LocalizedDoctrine lang={params.lang} />
       <CurriculumPath />
+      <CapitalismAssessment />
     </>
   );
 }
