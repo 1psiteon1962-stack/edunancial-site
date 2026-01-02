ed7535464e652cf024cc019a867ab09ee25c618c
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-01-02 14:49:02 -0800

    Update page.tsx

--- app/us/[lang]/page.tsx
+++ app/us/[lang]/page.tsx
@@ -4,19 +4,8 @@ import { notFound } from "next/navigation";
 import RegionCurriculum from "@/components/RegionCurriculum";
 import { isLanguage } from "@/lib/language";
 
-export default function Page({
-  params,
-}: {
-  params: { lang: string };
-}) {
-  if (!isLanguage(params.lang)) {
-    notFound();
-  }
+export default function Page({ params }: { params: { lang: string } }) {
+  if (!isLanguage(params.lang)) notFound();
 
-  return (
-    <RegionCurriculum
-      regionKey="us"
-      lang={params.lang}
-    />
-  );
+  return <RegionCurriculum regionKey="us" lang={params.lang} />;
 }
