3bb45b127bfbe1f6a44787a2d1928ff7ec4e06db
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-01-02 14:20:19 -0800

    Update page.tsx

--- app/asia-pacific/[lang]/page.tsx
+++ app/asia-pacific/[lang]/page.tsx
@@ -1,17 +1,8 @@
-// app/asia-pacific/[lang]/page.tsx
-import { notFound } from 'next/navigation';
-import RegionCurriculum from '@/components/RegionCurriculum';
-import { regionContent } from '@/lib/regionContent';
-import { isLanguage } from '@/lib/language';
+import { notFound } from "next/navigation";
+import RegionCurriculum from "@/components/RegionCurriculum";
+import { isLanguage } from "@/lib/language";
 
 export default function Page({ params }: { params: { lang: string } }) {
-  if (!isLanguage(params.lang)) return notFound();
-
-  return (
-    <RegionCurriculum
-      regionKey="asia-pacific"
-      lang={params.lang}
-      content={regionContent['asia-pacific'][params.lang]}
-    />
-  );
+  if (!isLanguage(params.lang)) notFound();
+  return <RegionCurriculum regionKey="asia-pacific" lang={params.lang} />;
 }
