07583e7e56908b59e3c7cb7e7f8a872424abd3da
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-01-02 19:53:44 -0800

    Update page.tsx

--- app/asia-pacific/[lang]/page.tsx
+++ app/asia-pacific/[lang]/page.tsx
@@ -2,13 +2,7 @@ import { notFound } from "next/navigation";
 import RegionCurriculum from "@/components/RegionCurriculum";
 import { isLanguage, Language } from "@/lib/language";
 
-type Params = {
-  params: {
-    lang: string;
-  };
-};
-
-export default function Page({ params }: Params) {
+export default function Page({ params }: { params: { lang: string } }) {
   if (!isLanguage(params.lang)) notFound();
 
   return (
