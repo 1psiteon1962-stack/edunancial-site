e27d67f8bc7baa285978e33650e5c74d1661e64d
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-01-03 08:37:27 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: "standalone",
+  experimental: {
+    instrumentationHook: true,
+  },
 };
 
 module.exports = nextConfig;
