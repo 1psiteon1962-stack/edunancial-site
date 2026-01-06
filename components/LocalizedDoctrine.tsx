f767daf68005019ab22d77e300b2da7da261eebc
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-01-05 22:56:36 -0800

    Update LocalizedDoctrine.tsx

--- components/LocalizedDoctrine.tsx
+++ components/LocalizedDoctrine.tsx
@@ -1,12 +1,23 @@
-// components/LocalizedDoctrine.tsx
+import { type Language, t } from "@/lib/i18n";
 
-import { t } from '@/lib/translator';
+type Props = {
+  lang: Language;
+};
 
-export default function LocalizedDoctrine() {
+export default function LocalizedDoctrine({ lang }: Props) {
   return (
-    <section style={{ marginTop: '2rem' }}>
-      <h2>{t('missionTitle')}</h2>
-      <p>{t('missionBody')}</p>
+    <section style={{ marginTop: "2rem" }}>
+      <h2>{t("missionTitle")}</h2>
+      <p>{t("missionBody")}</p>
+
+      <ul>
+        <li>{t("pillarAccess")}</li>
+        <li>{t("pillarStructure")}</li>
+        <li>{t("pillarProtection")}</li>
+        <li>{t("pillarScale")}</li>
+      </ul>
+
+      <p>{t("missionFooter")}</p>
     </section>
   );
 }
