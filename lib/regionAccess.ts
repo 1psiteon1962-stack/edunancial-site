06bf93014414a35fc95dc300679f58f3bba335a8
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-01-08 04:03:37 -0800

    Update regionAccess.ts

--- lib/regionAccess.ts
+++ lib/regionAccess.ts
@@ -1,13 +1,18 @@
+// lib/regionAccess.ts
+
 import { REGIONS, RegionCode } from "./regions.config";
 import { canAccessRegion } from "./navigationGuard";
 
+/**
+ * Returns region access metadata used by routing and UI.
+ */
 export function getRegionAccess(regionCode: RegionCode) {
   const region = REGIONS[regionCode];
 
   return {
     region,
-    allowed: canAccessRegion(regionCode, region),
-    enabled: region.enabled ?? true,
-    readinessKey: region.readinessKey ?? regionCode,
+    allowed: canAccessRegion(region),
+    enabled: region.enabled,
+    readinessKey: region.readinessKey,
   };
 }
