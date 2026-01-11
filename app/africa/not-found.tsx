39de2c2de99f8e181ba672d50f80637f2f0c66f3
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-01-11 14:42:54 -0800

    Delete app/africa directory

--- app/africa/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function AfricaNotFound() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h2>Page not found</h2>
-      <p>This resource is not available in the Africa mirror.</p>
-    </div>
-  );
-}
