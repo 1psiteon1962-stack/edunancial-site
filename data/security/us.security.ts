7f152d7b0acf38e9a3341fb8021cafdd42467c6d
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-01-11 21:12:13 -0800

    Create us.security.ts

---
+++ data/security/us.security.ts
@@ -0,0 +1,14 @@
+export const US_SECURITY = {
+  firewall: true,
+  ddosProtection: true,
+  apiIsolation: true,
+  encryptedPayments: true,
+  encryptedUserData: true,
+  auditLogs: true,
+  intrusionDetection: true,
+  complianceFrameworks: ["SOC2", "CCPA", "GDPR"],
+  cyberInsuranceReady: true,
+  ipProtection: true,
+  dataBackups: true,
+  multiRegionFailover: true
+};
