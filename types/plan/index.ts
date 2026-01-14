76482c6408921418064a9e65a6e85f918dd40868
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-01-13 19:44:39 -0800

    Update index.ts

--- types/plan/index.ts
+++ types/plan/index.ts
@@ -1,2 +1,16 @@
-export { PLANS, PLAN_LABELS } from "../plan";
-export type { PlanCode, Plan } from "../plan";
+import { PLANS } from "../plan";
+
+/**
+ * Canonical plan code type used everywhere
+ */
+export type PlanCode = keyof typeof PLANS;
+
+/**
+ * Full plan object type
+ */
+export type Plan = (typeof PLANS)[PlanCode];
+
+/**
+ * Re-export core data
+ */
+export { PLANS };
