468893e9e455cec9ef5967246c7a2af56fda066f
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-01-15 13:18:01 -0800

    Update us.apps.ts

--- data/apps/us.apps.ts
+++ data/apps/us.apps.ts
@@ -1,1 +1,9 @@
-export const usApps = []
+export type App = {
+  id: string
+  name: string
+}
+
+export const US_APPS: App[] = [
+  { id: "edunancial", name: "Edunancial" },
+  { id: "ultimadefensa", name: "Ultima Defensa Legal" }
+]
