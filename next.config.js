28c79c1b223abf798dc566df9a6cef40e7fc4dfe
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-01-15 08:44:28 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+
+  // Netlify/OpenNext-safe defaults:
+  output: "standalone",
+
+  // If you are using images, this avoids Netlify image optimizer issues.
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
