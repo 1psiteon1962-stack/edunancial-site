210eba9c91d179c9b81d20df0c161e2aea771759
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-01-15 13:18:47 -0800

    Update i18n.ts

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,3 +1,11 @@
+export const REGION_LANGUAGES = ["en", "es", "fr", "ar"] as const
+
+export type Language = (typeof REGION_LANGUAGES)[number]
+
+export function isLanguage(x: any): x is Language {
+  return REGION_LANGUAGES.includes(x)
+}
+
 export function t(key: string) {
   return key
 }
