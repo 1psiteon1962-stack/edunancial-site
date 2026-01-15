a460122e3dbdc389c69955dd13859ccc318c9c7c
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-01-15 12:59:28 -0800

    Update RegionCurriculum.tsx

--- components/RegionCurriculum.tsx
+++ components/RegionCurriculum.tsx
@@ -1,32 +1,3 @@
-import { Language } from "@/lib/i18n";
-
-export type Region =
-  | "us"
-  | "latam"
-  | "caribbean"
-  | "europe"
-  | "africa"
-  | "mena"
-  | "asia"
-  | "asia-pacific"
-  | "asia-emerging";
-
-type Props = {
-  region: Region;
-  lang: Language;
-  supportedLanguages: readonly Language[];
-};
-
-export default function RegionCurriculum({
-  region,
-  lang,
-  supportedLanguages,
-}: Props) {
-  return (
-    <section>
-      <h1>{region.toUpperCase()} Curriculum</h1>
-      <p>Language: {lang}</p>
-      <p>Supported Languages: {supportedLanguages.join(", ")}</p>
-    </section>
-  );
+export default function RegionCurriculum() {
+  return null
 }
