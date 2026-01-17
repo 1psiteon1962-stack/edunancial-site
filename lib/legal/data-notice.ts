cf598cda73a93c87760470287ce298f56c724c20
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-01-17 13:20:03 -0800

    Create data-notice.ts

---
+++ lib/legal/data-notice.ts
@@ -0,0 +1,9 @@
+export const DATA_COLLECTION_NOTICE = `
+We collect limited personal information for internal analytics,
+platform improvement, and business performance measurement.
+
+We do not collect full dates of birth.
+Age data is captured only in ranges.
+Information is not sold publicly and is used internally or
+in aggregated, anonymized form for strategic analysis.
+`;
