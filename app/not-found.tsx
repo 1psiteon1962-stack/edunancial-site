475b1a2763b4ba57276f5d91cc96e5d05e0ccf53
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-01-18 14:11:04 -0800

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main style={{ padding: "4rem", textAlign: "center" }}>
+      <h1>Page not found</h1>
+      <p>If you reached this page, routing is misconfigured.</p>
+    </main>
+  );
+}
