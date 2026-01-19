8b7a3de3f2a770e24c07f49e818dfe210511d47a
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-01-18 19:57:39 -0800

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Edunancial</h1>
+      <p>Site is live and running on server runtime.</p>
+    </main>
+  );
+}
