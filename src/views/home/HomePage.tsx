4e14017e275ef0f62b4cf6af6d6635e258783b8f
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-02-09 10:31:23 -0800

    Create HomePage.tsx

---
+++ src/views/home/HomePage.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function HomePage() {
+  return (
+    <main style={{ padding: 32 }}>
+      <h1>Welcome</h1>
+      <p>Home page loaded successfully.</p>
+    </main>
+  );
+}
