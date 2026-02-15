77c855ec24b0060c7fd0334664401c843cb20a0f
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-02-14 20:47:19 -0800

    Update RegionNav.tsx

--- components/RegionNav.tsx
+++ components/RegionNav.tsx
@@ -1,22 +1,21 @@
-// components/RegionNav.tsx
-
+import React from "react";
 import Link from "next/link";
 import { REGIONS } from "@/data/regions";
 
 export default function RegionNav() {
-  const enabledRegions = Object.entries(REGIONS).filter(
-    ([_, region]) => region.enabled
-  );
+  const regions = Object.values(REGIONS);
 
   return (
-    <nav style={{ padding: "1rem 0", borderBottom: "1px solid #ddd" }}>
-      <strong>Regions:</strong>{" "}
-      {enabledRegions.map(([key, region], index) => (
-        <span key={key}>
-          <Link href={`/${key}`}>{region.name}</Link>
-          {index < enabledRegions.length - 1 ? " | " : ""}
-        </span>
-      ))}
+    <nav>
+      <ul>
+        {regions.map((region) => (
+          <li key={region.code}>
+            <Link href={`/${region.code}`}>
+              {region.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
