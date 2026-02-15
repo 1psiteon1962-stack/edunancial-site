7eaf779b8e0caa96195018fc832b4a89f7bfac58
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-02-14 20:55:39 -0800

    Update user-profile.ts

--- lib/types/user-profile.ts
+++ lib/types/user-profile.ts
@@ -8,29 +8,7 @@ export type AgeRange =
   | "65_plus";
 
 export type BusinessStatus =
-  | "no_business"
-  | "informal_business"
-  | "formal_llc"
-  | "formal_corp"
-  | "other";
-
-export interface UserProfile {
-  firstName: string;
-  lastName: string;
-  email: string;
-  phone?: string;
-
-  country: string;
-  state?: string;
-  city?: string;
-  postalCode?: string;
-
-  ageRange: AgeRange;
-
-  hasBusiness: boolean;
-  businessName?: string;
-  businessJurisdiction?: string;
-  businessStatus?: BusinessStatus;
-
-  createdAt: string;
-}
+  | "none"
+  | "planning"
+  | "operating"
+  | "scaling";
