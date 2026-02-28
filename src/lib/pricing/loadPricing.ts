644ce7657896ef2875026122133ad52593483498
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-02-28 11:19:56 -0800

    Add pricing support for multiple regions

--- src/lib/pricing/loadPricing.ts
+++ src/lib/pricing/loadPricing.ts
@@ -13,6 +13,18 @@ export async function loadPricing(region: string): Promise<Pricing> {
   switch (region) {
     case "africa":
       return (await import("./africa")).AFRICA_PRICING;
+    case "asia":
+      return (await import("./asia")).ASIA_PRICING;
+    case "latam":
+      return (await import("./latam")).LATAM_PRICING;
+    case "europe":
+      return (await import("./europe")).EUROPE_PRICING;
+    case "middleeast":
+      return (await import("./middleEast")).MIDDLE_EAST_PRICING;
+    case "caribbean":
+      return (await import("./caribbean")).CARIBBEAN_PRICING;
+    case "oceania":
+      return (await import("./oceania")).OCEANIA_PRICING;
     case "us":
     default:
       return (await import("./us")).US_PRICING;
