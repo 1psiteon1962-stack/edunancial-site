62adb9092a643a3b216168ebd2ea26147f383e9a
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-02-27 16:10:57 -0800

    Implement getDictionary function for translations

---
+++ src/lib/i18n/getDictionary.ts
@@ -0,0 +1,22 @@
+import { LanguageCode } from "../regions";
+
+export async function getDictionary(lang: LanguageCode) {
+  switch (lang) {
+    case "es":
+      return (await import("./translations/es")).default;
+    case "ko":
+      return (await import("./translations/ko")).default;
+    case "ja":
+      return (await import("./translations/ja")).default;
+    case "tl":
+      return (await import("./translations/tl")).default;
+    case "ar":
+      return (await import("./translations/ar")).default;
+    case "pt":
+      return (await import("./translations/pt")).default;
+    case "fr":
+      return (await import("./translations/fr")).default;
+    default:
+      return (await import("./translations/en")).default;
+  }
+}
