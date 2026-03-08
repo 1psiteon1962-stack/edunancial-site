d1c99068017e952fba41e1cf78f4586517bba349
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-03-08 16:00:27 -0700

    Add index file to export components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+'use client';
+
+export { default as Hero } from "./Hero/Hero";
+export { default as CTA } from "./CTA/CTA";
+export { default as FeatureGrid } from "./FeatureGrid/FeatureGrid";
+export { default as Footer } from "./Footer/Footer";
+export { default as Navbar } from "./Navbar/Navbar";
