595b0b5627338ab2ef44ae9ccabe1d72dade304d
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-03-08 18:53:41 -0700

    Refactor Page component to use HomePage

--- app/(regions)/(root)/page.tsx
+++ app/(regions)/(root)/page.tsx
@@ -1,19 +1,5 @@
-import Hero from "@/components/Hero/Hero";
-import FeatureGrid from "@/components/FeatureGrid/FeatureGrid";
-import CTA from "@/components/CTA/CTA";
-import Footer from "@/components/Footer/Footer";
-import Navbar from "@/components/Navbar/Navbar";
-
-export const dynamic = "force-dynamic";
+import { HomePage } from "@/components/home/HomePage";
 
 export default function Page() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <FeatureGrid />
-      <CTA />
-      <Footer />
-    </>
-  );
+  return <HomePage />;
 }
