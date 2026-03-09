8305e4eeb3d2b50ad1cb6ee86307cf6768b85b68
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-03-08 18:06:40 -0700

    Refactor exports in index.ts to remove file names

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 'use client';
 
-export { default as Hero } from "./Hero/Hero";
-export { default as CTA } from "./CTA/CTA";
-export { default as FeatureGrid } from "./FeatureGrid/FeatureGrid";
-export { default as Footer } from "./Footer/Footer";
-export { default as Navbar } from "./Navbar/Navbar";
+export { default as Hero } from "./Hero";
+export { default as CTA } from "./CTA";
+export { default as FeatureGrid } from "./FeatureGrid";
+export { default as Footer } from "./Footer";
+export { default as Navbar } from "./Navbar";
