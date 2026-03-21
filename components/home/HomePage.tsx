c77e8478eb27a695f41d25544f12efc251c0f172
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-03-20 19:59:49 -0700

    Add HomePage component with header and description

---
+++ components/home/HomePage.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Edunancial</h1>
+      <p>Where Education and Financial Literacy Meet</p>
+    </main>
+  );
+}
