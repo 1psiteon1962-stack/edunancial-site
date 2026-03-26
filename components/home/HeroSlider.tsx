40faf013b28cfe1f5ff35ce7424c33b5a4896f56
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-03-25 19:47:40 -0700

    Use dynamic imports for Swiper in HeroSlider
    Refactored HeroSlider to use dynamic imports for Swiper components.

--- components/home/HeroSlider.tsx
+++ components/home/HeroSlider.tsx
@@ -1,33 +1,34 @@
 'use client';
 
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import dynamic from 'next/dynamic';
 
-// Import Swiper styles (safe in client)
-import 'swiper/css';
+const SwiperComponent = dynamic(
+  async () => {
+    const mod = await import('swiper/react');
+    return mod.Swiper;
+  },
+  { ssr: false }
+);
+
+const SwiperSlideComponent = dynamic(
+  async () => {
+    const mod = await import('swiper/react');
+    return mod.SwiperSlide;
+  },
+  { ssr: false }
+);
 
 export default function HeroSlider() {
   return (
-    <div style={{ width: '100%', height: '300px' }}>
-      <Swiper spaceBetween={50} slidesPerView={1}>
-        <SwiperSlide>
-          <div style={{ background: '#111', color: '#fff', padding: 40 }}>
-            Slide 1 – Edunancial
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div style={{ background: '#222', color: '#fff', padding: 40 }}>
-            Slide 2 – Learn. Build. Scale.
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <div style={{ background: '#333', color: '#fff', padding: 40 }}>
-            Slide 3 – Global Growth
-          </div>
-        </SwiperSlide>
-      </Swiper>
+    <div>
+      <SwiperComponent>
+        <SwiperSlideComponent>
+          <div>Slide 1</div>
+        </SwiperSlideComponent>
+        <SwiperSlideComponent>
+          <div>Slide 2</div>
+        </SwiperSlideComponent>
+      </SwiperComponent>
     </div>
   );
 }
