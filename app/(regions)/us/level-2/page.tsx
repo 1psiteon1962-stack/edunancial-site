4c42619c832cea2bca84a690af71ee05f9357f94
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-03-30 21:28:49 -0700

    Refactor AccessGate import and update content

--- app/(regions)/us/level-2/page.tsx
+++ app/(regions)/us/level-2/page.tsx
@@ -1,9 +1,9 @@
-import AccessGate from '@/components/AccessGate'
+import { AccessGate } from '@/components'
 
 export default function Page() {
   return (
     <AccessGate requiredLevel={2}>
-      <div>Level 2 Content</div>
+      <div>Level 2</div>
     </AccessGate>
   )
 }
