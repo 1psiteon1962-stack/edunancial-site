6ebebbcf62b1dbc497da271e40aa79d2057c8ea4
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-04-11 12:34:39 -0700

    Create site-config.ts

---
+++ src/lib/site-config.ts
@@ -0,0 +1,6 @@
+/**
+ * SAFE DEFAULT REGION CONFIG
+ * This prevents the root page from crashing if the previous slug
+ * did not match available region content.
+ */
+export const defaultRegionSlug = "us";
