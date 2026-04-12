6e2eda39d6518d669fb38ad0bdefabff5eec8d29
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-04-11 20:49:03 -0700

    Update GlobalLayout.tsx

--- components/GlobalLayout.tsx
+++ components/GlobalLayout.tsx
@@ -1,17 +1,3 @@
-import React from "react";
-
-export default function GlobalLayout({
-  children
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <div>
-      <header style={{ padding: 20, background: "#111", color: "#fff" }}>
-        Edunancial
-      </header>
-
-      <main>{children}</main>
-    </div>
-  );
+export default function GlobalLayout({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
 }
