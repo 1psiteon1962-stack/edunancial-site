fe1719668877cec94bb6209228871b21c508fa69
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-04-19 08:07:09 -0700

    Update PageViewTracker.tsx

--- src/components/kpi/PageViewTracker.tsx
+++ src/components/kpi/PageViewTracker.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function PageViewTracker() {
   useEffect(() => {
     try {
-      console.log("Tracking page:", window.location.pathname);
+      console.log("Page view:", window.location.pathname);
     } catch (err) {
       console.warn("Tracking error:", err);
     }
