fd27b663de858f975fedd68e464e4d2d8d36214f
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-05-03 19:36:40 -0700

    Refactor HomePage component into Page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,5 @@
-// src/app/page.tsx
+import HomePage from "@/views/home/HomePage";
 
-import { loadPricing } from "../lib/pricing/loadPricing";
-
-export default async function HomePage() {
-  const pricing = await loadPricing();
-
-  return (
-    <main>
-      <h1>Edunancial</h1>
-
-      <section>
-        {pricing.map((plan) => (
-          <div key={plan.id}>
-            <h2>{plan.name}</h2>
-            <p>
-              {plan.currency} ${plan.price}
-            </p>
-            <ul>
-              {plan.features.map((feature) => (
-                <li key={feature}>{feature}</li>
-              ))}
-            </ul>
-          </div>
-        ))}
-      </section>
-    </main>
-  );
+export default function Page() {
+  return <HomePage />;
 }
