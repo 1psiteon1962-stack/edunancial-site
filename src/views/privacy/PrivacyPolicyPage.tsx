c7dcca5af0569af93107b553366ffc1ff6ad4eef
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-02 21:12:15 -0700

    Add PrivacyPolicyPage component

---
+++ src/views/privacy/PrivacyPolicyPage.tsx
@@ -0,0 +1,27 @@
+export default function PrivacyPolicyPage() {
+  return (
+    <main style={{ maxWidth: "1000px", margin: "0 auto", padding: "20px" }}>
+      <h1>Privacy Policy</h1>
+
+      <p>
+        Edunancial respects your privacy.
+      </p>
+
+      <p>
+        We may collect information voluntarily submitted
+        through forms, memberships, purchases,
+        and newsletter subscriptions.
+      </p>
+
+      <p>
+        Information is used solely for business operations,
+        customer service, content delivery,
+        and communications.
+      </p>
+
+      <p>
+        We do not sell personal information.
+      </p>
+    </main>
+  );
+}
