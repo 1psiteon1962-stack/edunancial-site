507205f2064aa5780689b1316aa04966803f74a4
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-20 17:51:43 -0700

    Add FutureAfrica component with initiative details

---
+++ src/components/FutureAfrica.tsx
@@ -0,0 +1,101 @@
+export default function FutureAfrica(){
+
+return(
+
+<section
+
+style={{
+
+padding:"60px",
+
+maxWidth:"1000px",
+
+margin:"0 auto",
+
+}}
+
+>
+
+<h2>
+
+Africa Initiative
+
+</h2>
+
+<p>
+
+Edunancial believes
+
+there are talented
+
+entrepreneurs
+
+throughout Africa
+
+who need:
+
+</p>
+
+<ul>
+
+<li>
+
+Financial Literacy
+
+</li>
+
+<li>
+
+Mentorship
+
+</li>
+
+<li>
+
+Business Education
+
+</li>
+
+<li>
+
+Accountability
+
+</li>
+
+<li>
+
+Responsible Access To Capital
+
+</li>
+
+</ul>
+
+<p>
+
+Our long-term goal
+
+is to help entrepreneurs
+
+build sustainable businesses
+
+that create wealth,
+
+jobs,
+
+and opportunity.
+
+</p>
+
+<p>
+
+Programs are currently
+
+under development.
+
+</p>
+
+</section>
+
+);
+
+}
