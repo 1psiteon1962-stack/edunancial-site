d5232767f49d1ba56a3c4f45efb9be1a63afd4fb
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-20 20:59:10 -0700

    Add MembershipCheckout component for membership options

---
+++ src/components/MembershipCheckout.tsx
@@ -0,0 +1,77 @@
+export default function MembershipCheckout(){
+
+return(
+
+<section
+
+style={{
+
+padding:"60px",
+
+maxWidth:"1000px",
+
+margin:"0 auto",
+
+}}
+
+>
+
+<h2>
+
+Membership Checkout
+
+</h2>
+
+<h3>
+
+Basic
+
+</h3>
+
+<p>
+
+$9.99 Monthly
+
+</p>
+
+<h3>
+
+Gold
+
+</h3>
+
+<p>
+
+$29.99 Monthly
+
+</p>
+
+<h3>
+
+Mentor
+
+</h3>
+
+<p>
+
+Future Program
+
+</p>
+
+<p>
+
+Membership terms,
+
+refund policies,
+
+and disclaimers
+
+apply.
+
+</p>
+
+</section>
+
+);
+
+}
