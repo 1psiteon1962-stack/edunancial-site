453c294bc524859856d94f115e45de9a4a3724ae
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-20 20:57:36 -0700

    Add SquareCheckout component for payment integration

---
+++ src/components/SquareCheckout.tsx
@@ -0,0 +1,73 @@
+export default function SquareCheckout(){
+
+return(
+
+<section
+
+style={{
+
+padding:"60px",
+
+maxWidth:"800px",
+
+margin:"0 auto",
+
+}}
+
+>
+
+<h2>
+
+Square Checkout
+
+</h2>
+
+<p>
+
+Square integration
+
+will allow users
+
+to purchase:
+
+</p>
+
+<ul>
+
+<li>
+
+Memberships
+
+</li>
+
+<li>
+
+Courses
+
+</li>
+
+<li>
+
+Books
+
+</li>
+
+<li>
+
+Downloads
+
+</li>
+
+</ul>
+
+<p>
+
+Integration coming soon.
+
+</p>
+
+</section>
+
+);
+
+}
