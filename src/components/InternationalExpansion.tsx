0485e91c57bfebd3fbf3e34288875fed419e135b
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-20 17:51:09 -0700

    Add InternationalExpansion component with content

---
+++ src/components/InternationalExpansion.tsx
@@ -0,0 +1,109 @@
+export default function
+
+InternationalExpansion(){
+
+return(
+
+<section
+
+style={{
+
+padding:"60px",
+
+background:"#f5f5f5",
+
+}}
+
+>
+
+<div
+
+style={{
+
+maxWidth:"1000px",
+
+margin:"0 auto",
+
+}}
+
+>
+
+<h2>
+
+International Expansion
+
+</h2>
+
+<p>
+
+Edunancial intends
+
+to expand internationally
+
+over time.
+
+</p>
+
+<h3>
+
+Planned Regions
+
+</h3>
+
+<ul>
+
+<li>
+
+North America
+
+</li>
+
+<li>
+
+Africa
+
+</li>
+
+<li>
+
+Latin America
+
+and Caribbean
+
+</li>
+
+<li>
+
+English-Speaking Asia
+
+</li>
+
+<li>
+
+Europe
+
+</li>
+
+</ul>
+
+<p>
+
+Expansion timelines
+
+and services
+
+may vary
+
+by country
+
+and applicable laws.
+
+</p>
+
+</div>
+
+</section>
+
+);
+
+}
