41f7c8fcf6a3fcadd3a4c4f72425595ed2ebe3b2
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-20 20:58:16 -0700

    Add PayPalCheckout component for payment options

---
+++ src/components/PayPalCheckout.tsx
@@ -0,0 +1,75 @@
+export default function PayPalCheckout(){
+
+return(
+
+<section
+
+style={{
+
+padding:"60px",
+
+maxWidth:"800px",
+
+margin:"0 auto",
+
+}}
+
+>
+
+<h2>
+
+PayPal Checkout
+
+</h2>
+
+<p>
+
+PayPal support
+
+may be added
+
+to provide
+
+additional payment options.
+
+</p>
+
+<p>
+
+Future purchases may include:
+
+</p>
+
+<ul>
+
+<li>
+
+Memberships
+
+</li>
+
+<li>
+
+Courses
+
+</li>
+
+<li>
+
+Books
+
+</li>
+
+<li>
+
+Downloads
+
+</li>
+
+</ul>
+
+</section>
+
+);
+
+}
