cf09bc97b0cb75d2505527575df4ea56427e61f6
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-20 21:00:20 -0700

    Add MicroGrantProgram component with program details

---
+++ src/components/MicroGrantProgram.tsx
@@ -0,0 +1,119 @@
+export default function MicroGrantProgram(){
+
+return(
+
+<section
+
+style={{
+
+padding:"60px",
+
+background:"#f5f5f5",
+
+}}
+
+>
+
+<div
+
+style={{
+
+maxWidth:"1000px",
+
+margin:"0 auto",
+
+}}
+
+>
+
+<h2>
+
+Micro Grant Program
+
+</h2>
+
+<p>
+
+Edunancial intends
+
+to explore
+
+small grant programs
+
+for entrepreneurs
+
+who demonstrate:
+
+</p>
+
+<ul>
+
+<li>
+
+Financial Literacy
+
+</li>
+
+<li>
+
+Business Knowledge
+
+</li>
+
+<li>
+
+Recordkeeping
+
+</li>
+
+<li>
+
+Accountability
+
+</li>
+
+</ul>
+
+<p>
+
+Potential grant levels:
+
+</p>
+
+<ul>
+
+<li>
+
+$100
+
+</li>
+
+<li>
+
+$250
+
+</li>
+
+<li>
+
+$500
+
+</li>
+
+</ul>
+
+<p>
+
+Programs are under development.
+
+No grants are currently guaranteed.
+
+</p>
+
+</div>
+
+</section>
+
+);
+
+}
