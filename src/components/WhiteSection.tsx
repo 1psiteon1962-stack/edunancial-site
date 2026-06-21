080bf9bd6634aa8cb0f6e1887b8069de2e22ba44
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-20 17:16:36 -0700

    Create WhiteSection component for financial assets

---
+++ src/components/WhiteSection.tsx
@@ -0,0 +1,51 @@
+export default function WhiteSection(){
+
+return(
+
+<section
+
+style={
+
+{
+
+padding:"50px",
+
+background:"#ffffff",
+
+}
+
+}
+
+>
+
+<h2>
+
+WHITE
+
+Paper Assets
+
+</h2>
+
+<ul>
+
+<li>Budgeting</li>
+
+<li>Stocks</li>
+
+<li>ETFs</li>
+
+<li>Options</li>
+
+<li>Precious Metals</li>
+
+<li>Retirement Accounts</li>
+
+<li>Risk Management</li>
+
+</ul>
+
+</section>
+
+);
+
+}
