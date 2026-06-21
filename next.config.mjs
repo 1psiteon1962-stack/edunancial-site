c4971d5f8afedfac22e6911fcbaec3d5711ae6b4
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-21 11:48:58 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-
   reactStrictMode: true,
-
   images: {
-
-    unoptimized: true,
-
-  },
-
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
