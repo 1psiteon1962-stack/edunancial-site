72433f879df303181d179e4c70bac1206ec000f8
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-21 02:38:38 -0700

    Enable experimental typed routes in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+
+  experimental: {
+    typedRoutes: true
+  },
+
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
