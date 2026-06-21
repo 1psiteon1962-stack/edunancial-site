e4d0d3e78ab2729bcf266c8edef00f67d481aa72
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-20 17:22:58 -0700

    Refactor Page component to use HomePage structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,43 @@
-import HomePage from "@/views/home/HomePage";
+import Hero from "../components/Hero";
 
-export default function Page() {
-  return <HomePage />;
+import WhiteSection from "../components/WhiteSection";
+
+import RedSection from "../components/RedSection";
+
+import BlueSection from "../components/BlueSection";
+
+import LevelsOverview from "../components/LevelsOverview";
+
+import SponsorPreview from "../components/SponsorPreview";
+
+import CallToAction from "../components/CallToAction";
+
+import FinancialDisclaimer from "../components/FinancialDisclaimer";
+
+export default function HomePage(){
+
+return(
+
+<main>
+
+<Hero />
+
+<WhiteSection />
+
+<RedSection />
+
+<BlueSection />
+
+<LevelsOverview />
+
+<SponsorPreview />
+
+<CallToAction />
+
+<FinancialDisclaimer />
+
+</main>
+
+);
+
 }
