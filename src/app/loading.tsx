8c1692649803c09089fb30ca26ecfdff179ede75
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-20 17:48:34 -0700

    Add Loading component for loading state

---
+++ src/app/loading.tsx
@@ -0,0 +1,35 @@
+export default function Loading(){
+
+return(
+
+<main
+
+style={{
+
+padding:"100px",
+
+textAlign:"center",
+
+fontFamily:"Arial,sans-serif",
+
+}}
+
+>
+
+<h1>
+
+Loading...
+
+</h1>
+
+<p>
+
+Please wait.
+
+</p>
+
+</main>
+
+);
+
+}
