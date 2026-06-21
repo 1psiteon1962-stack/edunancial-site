cbc6886bee6aeca75a18964959ea01ecc23bb22e
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-20 21:00:57 -0700

    Add MicroLoanProgram component with loan details

---
+++ src/components/MicroLoanProgram.tsx
@@ -0,0 +1,123 @@
+export default function MicroLoanProgram(){
+
+return(
+
+<section
+
+style={{
+
+padding:"60px",
+
+maxWidth:"1000px",
+
+margin:"0 auto",
+
+}}
+
+>
+
+<h2>
+
+Micro Loan Program
+
+</h2>
+
+<p>
+
+Edunancial is exploring
+
+future micro loan programs
+
+for entrepreneurs
+
+who demonstrate:
+
+</p>
+
+<ul>
+
+<li>
+
+Financial Literacy
+
+</li>
+
+<li>
+
+Business Experience
+
+</li>
+
+<li>
+
+Profitability
+
+</li>
+
+<li>
+
+Recordkeeping
+
+</li>
+
+<li>
+
+Mentorship Participation
+
+</li>
+
+</ul>
+
+<p>
+
+Potential loan levels:
+
+</p>
+
+<ul>
+
+<li>
+
+$500
+
+</li>
+
+<li>
+
+$1,000
+
+</li>
+
+<li>
+
+$2,500
+
+</li>
+
+<li>
+
+$5,000
+
+</li>
+
+</ul>
+
+<p>
+
+Programs are under development.
+
+Nothing on this page
+
+constitutes a loan offer,
+
+investment offering,
+
+or guarantee of funding.
+
+</p>
+
+</section>
+
+);
+
+}
