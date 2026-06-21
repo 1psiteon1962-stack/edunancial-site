2ece9a90e0fba62e2747bc737bf580570707ebdd
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-20 17:26:44 -0700

    Add SponsorPreview component with program details

---
+++ src/components/SponsorPreview.tsx
@@ -0,0 +1,119 @@
+export default function SponsorPreview(){
+
+return(
+
+<section
+
+style={
+
+{
+
+padding:"60px",
+
+background:"#f5f5f5",
+
+}
+
+}
+
+>
+
+<div
+
+style={
+
+{
+
+maxWidth:"1000px",
+
+margin:"0 auto",
+
+}
+
+}
+
+>
+
+<h2>
+
+Sponsor Entrepreneurs
+
+</h2>
+
+<p>
+
+Edunancial intends to connect mentors,
+
+entrepreneurs,
+
+and sponsors
+
+through education,
+
+accountability,
+
+and access to opportunity.
+
+</p>
+
+<h3>
+
+Future Programs
+
+</h3>
+
+<ul>
+
+<li>
+
+Financial Literacy
+
+</li>
+
+<li>
+
+Business Education
+
+</li>
+
+<li>
+
+Mentorship
+
+</li>
+
+<li>
+
+Micro Grants
+
+</li>
+
+<li>
+
+Micro Loans
+
+</li>
+
+<li>
+
+Entrepreneur Sponsorship
+
+</li>
+
+</ul>
+
+<p>
+
+Our goal is not merely to teach wealth.
+
+Our goal is to help create it.
+
+</p>
+
+</div>
+
+</section>
+
+);
+
+}
