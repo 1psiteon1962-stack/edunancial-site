e06a3223f125300a85115279b5716290f95b0538
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-20 17:17:25 -0700

    Create RedSection component for real estate listings

---
+++ src/components/RedSection.tsx
@@ -0,0 +1,49 @@
+export default function RedSection(){
+
+return(
+
+<section
+
+style={
+
+{
+
+padding:"50px",
+
+background:"#fafafa",
+
+}
+
+}
+
+>
+
+<h2>
+
+RED
+
+Real Estate
+
+</h2>
+
+<ul>
+
+<li>Rental Properties</li>
+
+<li>Tax Liens</li>
+
+<li>Tax Deeds</li>
+
+<li>Creative Financing</li>
+
+<li>1031 Exchanges</li>
+
+<li>Commercial Real Estate</li>
+
+</ul>
+
+</section>
+
+);
+
+}
