47dc5fead058fa238d5df8f2f104a962000cd7f8
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-20 17:21:23 -0700

    Add Navbar and Footer to RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,45 @@
 import "./globals.css";
-import type { ReactNode } from "react";
+
+import Navbar from "../components/Navbar";
+
+import Footer from "../components/Footer";
+
+export const metadata = {
+
+title:"Edunancial",
+
+description:
+
+"Edunancial is a Financial Literacy Platform dedicated to helping individuals understand money, business, real estate, paper assets, and entrepreneurship.",
+
+};
 
 export default function RootLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+
+children,
+
+}:{
+
+children:React.ReactNode;
+
+}){
+
+return(
+
+<html lang="en">
+
+<body>
+
+<Navbar />
+
+{children}
+
+<Footer />
+
+</body>
+
+</html>
+
+);
+
 }
