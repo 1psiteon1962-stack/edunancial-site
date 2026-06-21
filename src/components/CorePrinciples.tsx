4588f05ab49cf0f44269bc9c6450d324f5d107e3
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-20 17:49:46 -0700

    Add CorePrinciples component with key principles

---
+++ src/components/CorePrinciples.tsx
@@ -0,0 +1,83 @@
+export default function CorePrinciples(){
+
+return(
+
+<section
+
+style={{
+
+padding:"60px",
+
+background:"#f5f5f5",
+
+}}
+
+>
+
+<div
+
+style={{
+
+maxWidth:"1000px",
+
+margin:"0 auto",
+
+}}
+
+>
+
+<h2>
+
+Core Principles
+
+</h2>
+
+<ul>
+
+<li>
+
+Every dollar has a job.
+
+</li>
+
+<li>
+
+Profit is not greed.
+
+Profit is survival.
+
+</li>
+
+<li>
+
+No numbers equals no valid response.
+
+</li>
+
+<li>
+
+You can buy attention.
+
+You cannot buy trust.
+
+</li>
+
+<li>
+
+Success is measured by
+
+how many people you help
+
+create wealth.
+
+</li>
+
+</ul>
+
+</div>
+
+</section>
+
+);
+
+}
