8fccef3029dc7df6dee5e061de3ce7dea3e96fe2
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-20 20:55:11 -0700

    Add EmailSignup component for newsletter subscription

---
+++ src/components/EmailSignup.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+export default function EmailSignup() {
+
+return (
+
+<section
+
+style={{
+
+padding:"60px 20px",
+
+maxWidth:"800px",
+
+margin:"0 auto",
+
+}}
+
+>
+
+<h2>
+
+Stay Connected
+
+</h2>
+
+<p>
+
+Receive updates regarding:
+
+</p>
+
+<ul>
+
+<li>Financial Literacy</li>
+
+<li>Business Education</li>
+
+<li>Real Estate</li>
+
+<li>Paper Assets</li>
+
+<li>Mentorship Programs</li>
+
+<li>Future Entrepreneur Programs</li>
+
+</ul>
+
+<form>
+
+<input
+
+type="email"
+
+placeholder="Email Address"
+
+/>
+
+<br />
+
+<br />
+
+<button>
+
+Subscribe
+
+</button>
+
+</form>
+
+<p>
+
+You may unsubscribe at any time.
+
+</p>
+
+</section>
+
+);
+
+}
