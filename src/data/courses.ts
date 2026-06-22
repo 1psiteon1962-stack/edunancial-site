5ed7b37336c975990a8f943f5b6db9f285bc7e95
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-22 00:22:38 -0700

    Add courses data structure with course details

---
+++ src/data/courses.ts
@@ -0,0 +1,30 @@
+export const courses = [
+
+{
+id: 1,
+
+title: "RED - Real Estate",
+
+description:
+"Rental Properties, Tax Liens, Tax Deeds, Creative Financing and 1031 Exchanges."
+},
+
+{
+id: 2,
+
+title: "WHITE - Paper Assets",
+
+description:
+"Budgeting, Stocks, ETFs, Options, Precious Metals and Retirement Accounts."
+},
+
+{
+id: 3,
+
+title: "BLUE - Business",
+
+description:
+"Entrepreneurship, Marketing, KPIs, Inventory, Pricing and Profit."
+}
+
+];
