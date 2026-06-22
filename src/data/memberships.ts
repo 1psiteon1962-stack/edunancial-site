09009e2ee91258b124aebc822df08b4012cb83d9
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-21 23:20:50 -0700

    Add memberships data structure

---
+++ src/data/memberships.ts
@@ -0,0 +1,76 @@
+export const memberships = [
+
+  {
+    name: "Entry",
+
+    monthly: "$9.99",
+
+    description:
+      "Start learning financial literacy.",
+
+    features: [
+
+      "Blog",
+
+      "Pamphlets",
+
+      "Monthly Livestream",
+
+      "Selected Lessons",
+
+      "Free Downloads"
+
+    ]
+  },
+
+  {
+    name: "Standard",
+
+    monthly: "$29",
+
+    description:
+      "The primary Edunancial membership.",
+
+    features: [
+
+      "Financial Literacy 101",
+
+      "Economic Self Defense",
+
+      "Blue",
+
+      "White",
+
+      "Red",
+
+      "Downloads",
+
+      "Audio Lessons"
+
+    ]
+  },
+
+  {
+    name: "Premium",
+
+    monthly: "$59",
+
+    description:
+      "Everything Edunancial offers.",
+
+    features: [
+
+      "Everything in Standard",
+
+      "Quarterly Q&A",
+
+      "Advanced Material",
+
+      "Early Releases",
+
+      "Premium Downloads"
+
+    ]
+  }
+
+];
