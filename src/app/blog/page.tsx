acef33b8c04c0e4133f80b9a6a4f0fd064a9ab64
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-21 23:35:04 -0700

    Create BlogPage component with initial content

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,57 @@
+export default function BlogPage() {
+
+  return (
+
+    <main
+
+      style={{
+
+        maxWidth: "1100px",
+
+        margin: "0 auto",
+
+        padding: "60px 20px"
+
+      }}
+
+    >
+
+      <h1>
+
+        Blog
+
+      </h1>
+
+      <p>
+
+        Thoughts on:
+
+      </p>
+
+      <ul>
+
+        <li>Financial Literacy</li>
+
+        <li>Economic Self Defense</li>
+
+        <li>Business Ownership</li>
+
+        <li>Real Estate</li>
+
+        <li>Paper Assets</li>
+
+        <li>Entrepreneurship</li>
+
+      </ul>
+
+      <p>
+
+        Articles coming soon.
+
+      </p>
+
+    </main>
+
+  );
+
+}
