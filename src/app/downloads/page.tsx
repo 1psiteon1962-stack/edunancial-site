19f4aa4bb8e471190e4e8ba4fa86b3ef86d3e14d
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-21 23:26:12 -0700

    Add DownloadsPage component with download links

---
+++ src/app/downloads/page.tsx
@@ -0,0 +1,93 @@
+export default function DownloadsPage() {
+
+  return (
+
+    <main
+
+      style={{
+
+        maxWidth: "1100px",
+
+        margin: "0 auto",
+
+        padding: "60px 20px"
+
+      }}
+
+    >
+
+      <h1>
+
+        Downloads
+
+      </h1>
+
+      <p>
+
+        Practical tools you can use immediately.
+
+      </p>
+
+      <ul>
+
+        <li>
+
+          10 Rules to Build Wealth
+
+        </li>
+
+        <li>
+
+          Budget Worksheet
+
+        </li>
+
+        <li>
+
+          Emergency Fund Worksheet
+
+        </li>
+
+        <li>
+
+          Net Worth Tracker
+
+        </li>
+
+        <li>
+
+          Profit Calculator
+
+        </li>
+
+        <li>
+
+          Investment Tracker
+
+        </li>
+
+        <li>
+
+          Mortgage Worksheet
+
+        </li>
+
+        <li>
+
+          Rental Property Worksheet
+
+        </li>
+
+        <li>
+
+          Tax Lien Checklist
+
+        </li>
+
+      </ul>
+
+    </main>
+
+  );
+
+}
