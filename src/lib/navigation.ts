cb9f72a05fbcc069dd89b2cdb8b2dabebcf9560f
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-27 13:22:35 -0700

    Add navigation array with links and titles

---
+++ src/lib/navigation.ts
@@ -0,0 +1,38 @@
+export const navigation = [
+
+  {
+    title: "Home",
+    href: "/",
+  },
+
+  {
+    title: "Books",
+    href: "/books",
+  },
+
+  {
+    title: "Financial Terms",
+    href: "/terms",
+  },
+
+  {
+    title: "Courses",
+    href: "/courses",
+  },
+
+  {
+    title: "Membership",
+    href: "/membership",
+  },
+
+  {
+    title: "Downloads",
+    href: "/downloads",
+  },
+
+  {
+    title: "My Account",
+    href: "/account",
+  },
+
+];
