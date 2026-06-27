10bd7646e37c9b6be2da865a332fac0b218c785b
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-27 11:30:06 -0700

    Add SectionDivider component for visual separation

---
+++ src/components/SectionDivider.tsx
@@ -0,0 +1,7 @@
+export default function SectionDivider() {
+return (
+<div className="max-w-7xl mx-auto px-6 py-12">
+<div className="h-px bg-gradient-to-r from-red-600 via-white to-blue-600"></div>
+</div>
+);
+}
