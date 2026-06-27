16d3e2aac34f5826001cbb93edbc2362eab1b221
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-27 12:14:52 -0700

    Add HomepageLayout component with multiple sections

---
+++ src/components/HomepageLayout.tsx
@@ -0,0 +1,45 @@
+import HeroBanner from "./HeroBanner";
+import HomepageStory from "./HomepageStory";
+import HomepageProof from "./HomepageProof";
+import ThreePillars from "./ThreePillars";
+import HomepageMission from "./HomepageMission";
+import HomepageJourney from "./HomepageJourney";
+import HomepageBooks from "./HomepageBooks";
+import HomepageTerms from "./HomepageTerms";
+import HomepageMembership from "./HomepageMembership";
+import HomepageFinalCTA from "./HomepageFinalCTA";
+import Footer from "./Footer";
+
+export default function HomepageLayout() {
+
+  return (
+
+    <>
+
+      <HeroBanner />
+
+      <HomepageStory />
+
+      <HomepageProof />
+
+      <ThreePillars />
+
+      <HomepageMission />
+
+      <HomepageJourney />
+
+      <HomepageBooks />
+
+      <HomepageTerms />
+
+      <HomepageMembership />
+
+      <HomepageFinalCTA />
+
+      <Footer />
+
+    </>
+
+  );
+
+}
