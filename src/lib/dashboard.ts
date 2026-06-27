c0599343bb1c95b034faa8a2392418270622544e
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-27 14:28:57 -0700

    Add DashboardStats interface and initial data

---
+++ src/lib/dashboard.ts
@@ -0,0 +1,31 @@
+export interface DashboardStats {
+
+  totalBooks: number;
+
+  totalTerms: number;
+
+  totalCourses: number;
+
+  totalMembers: number;
+
+  totalSales: number;
+
+  totalRevenue: number;
+
+}
+
+export const dashboardStats: DashboardStats = {
+
+  totalBooks: 1,
+
+  totalTerms: 3,
+
+  totalCourses: 0,
+
+  totalMembers: 0,
+
+  totalSales: 0,
+
+  totalRevenue: 0,
+
+};
