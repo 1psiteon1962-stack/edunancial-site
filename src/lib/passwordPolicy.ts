76a19488bfe58896fe67350acd6031d733674438
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 13:50:29 -0700

    Add password policy configuration

---
+++ src/lib/passwordPolicy.ts
@@ -0,0 +1,15 @@
+export const PASSWORD_POLICY={
+
+minimumLength:12,
+
+requireUppercase:true,
+
+requireLowercase:true,
+
+requireNumber:true,
+
+requireSpecialCharacter:true,
+
+maximumAgeDays:180
+
+};
