745975376753852310fd6bfe429940abf7236363
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 12:51:51 -0700

    Add ABTest interface for A/B testing

---
+++ src/lib/abTestingTypes.ts
@@ -0,0 +1,11 @@
+export interface ABTest{
+id:string;
+name:string;
+variantA:string;
+variantB:string;
+viewsA:number;
+viewsB:number;
+conversionsA:number;
+conversionsB:number;
+active:boolean;
+}
