11c6e91f66f6ee317359e18b8ca20b7490064c0e
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 13:20:04 -0700

    Add RegionKPI interface for regional metrics

---
+++ src/lib/regionKpiTypes.ts
@@ -0,0 +1,25 @@
+export interface RegionKPI{
+
+region:string;
+
+countries:number;
+
+users:number;
+
+customers:number;
+
+revenue:number;
+
+profit:number;
+
+expenses:number;
+
+ltv:number;
+
+cac:number;
+
+conversionRate:number;
+
+growth:number;
+
+}
