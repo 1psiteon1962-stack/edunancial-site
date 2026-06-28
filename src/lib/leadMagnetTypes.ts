934b9e45d3d49ccc21721f4d9cdce85b290cf35d
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 12:21:47 -0700

    Add LeadMagnet interface definition

---
+++ src/lib/leadMagnetTypes.ts
@@ -0,0 +1,25 @@
+export interface LeadMagnet{
+
+id:string;
+
+title:string;
+
+description:string;
+
+fileUrl:string;
+
+imageUrl:string;
+
+language:string;
+
+region:string;
+
+active:boolean;
+
+downloads:number;
+
+conversions:number;
+
+revenue:number;
+
+}
