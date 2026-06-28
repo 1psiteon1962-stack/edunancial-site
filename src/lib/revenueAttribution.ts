1e4bcb47a92fa73a816c41930a3f36f24d0f38c0
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 13:06:39 -0700

    Add RevenueAttribution interface definition

---
+++ src/lib/revenueAttribution.ts
@@ -0,0 +1,25 @@
+export interface RevenueAttribution{
+
+productId:string;
+
+productName:string;
+
+country:string;
+
+region:string;
+
+language:string;
+
+campaign:string;
+
+grossRevenue:number;
+
+netRevenue:number;
+
+profit:number;
+
+margin:number;
+
+date:string;
+
+}
