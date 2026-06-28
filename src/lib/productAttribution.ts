01e8177a3dee8920a8b56d9be386c73b74428d1f
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 13:01:08 -0700

    Add ProductAttribution interface definition

---
+++ src/lib/productAttribution.ts
@@ -0,0 +1,21 @@
+export interface ProductAttribution{
+
+productId:string;
+
+productName:string;
+
+leadMagnet:string;
+
+campaign:string;
+
+customers:number;
+
+sales:number;
+
+revenue:number;
+
+profit:number;
+
+ltv:number;
+
+}
