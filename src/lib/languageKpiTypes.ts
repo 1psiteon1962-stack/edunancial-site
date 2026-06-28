008ecbcb507b909f496245ab35423cf6a16f8894
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 13:21:14 -0700

    Add LanguageKPI interface for language metrics

---
+++ src/lib/languageKpiTypes.ts
@@ -0,0 +1,23 @@
+export interface LanguageKPI{
+
+language:string;
+
+users:number;
+
+customers:number;
+
+emails:number;
+
+downloads:number;
+
+books:number;
+
+courses:number;
+
+revenue:number;
+
+profit:number;
+
+conversionRate:number;
+
+}
