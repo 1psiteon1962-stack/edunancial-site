33433726ed4955af0f76d05aeb9707a3a976e912
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 13:26:34 -0700

    Add AnnualKPI interface for annual metrics

---
+++ src/lib/annualKpiEngine.ts
@@ -0,0 +1,25 @@
+export interface AnnualKPI{
+
+year:number;
+
+users:number;
+
+customers:number;
+
+books:number;
+
+courses:number;
+
+memberships:number;
+
+revenue:number;
+
+expenses:number;
+
+profit:number;
+
+margin:number;
+
+growth:number;
+
+}
