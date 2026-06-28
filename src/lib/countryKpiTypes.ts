3cc7c0546953a01df63d4d59d4944c09451fdd28
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 13:20:41 -0700

    Add CountryKPI interface for country metrics

---
+++ src/lib/countryKpiTypes.ts
@@ -0,0 +1,25 @@
+export interface CountryKPI{
+
+country:string;
+
+language:string;
+
+users:number;
+
+customers:number;
+
+books:number;
+
+courses:number;
+
+memberships:number;
+
+revenue:number;
+
+profit:number;
+
+expenses:number;
+
+margin:number;
+
+}
