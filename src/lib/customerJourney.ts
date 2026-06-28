495561579a20e54ba56cb68db7fabf93786a7db4
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 12:26:03 -0700

    Add CustomerJourney interface for customer data

---
+++ src/lib/customerJourney.ts
@@ -0,0 +1,25 @@
+export interface CustomerJourney{
+
+subscriberId:string;
+
+joined:string;
+
+leadMagnet:string;
+
+emailsOpened:number;
+
+emailsClicked:number;
+
+booksPurchased:number;
+
+coursesPurchased:number;
+
+membership:boolean;
+
+totalRevenue:number;
+
+ltv:number;
+
+cac:number;
+
+}
