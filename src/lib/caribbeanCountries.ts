a842de5c01d63762a153295c8d232c0f028ae126
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-27 18:08:13 -0700

    Add list of Caribbean countries

---
+++ src/lib/caribbeanCountries.ts
@@ -0,0 +1,30 @@
+export const CARIBBEAN_COUNTRIES = [
+  "AI", // Anguilla
+  "AG", // Antigua and Barbuda
+  "AW", // Aruba
+  "BS", // Bahamas
+  "BB", // Barbados
+  "BQ", // Caribbean Netherlands
+  "VG", // British Virgin Islands
+  "KY", // Cayman Islands
+  "CU", // Cuba
+  "CW", // Curaçao
+  "DM", // Dominica
+  "DO", // Dominican Republic
+  "GD", // Grenada
+  "GP", // Guadeloupe
+  "HT", // Haiti
+  "JM", // Jamaica
+  "MQ", // Martinique
+  "MS", // Montserrat
+  "PR", // Puerto Rico
+  "BL", // Saint Barthélemy
+  "KN", // Saint Kitts and Nevis
+  "LC", // Saint Lucia
+  "MF", // Saint Martin
+  "VC", // Saint Vincent and the Grenadines
+  "SX", // Sint Maarten
+  "TT", // Trinidad and Tobago
+  "TC", // Turks and Caicos
+  "VI"  // U.S. Virgin Islands
+];
