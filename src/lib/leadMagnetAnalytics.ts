2fe376bd81899085a7aada33c68e1d4b917aaa31
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 13:07:20 -0700

    Add LeadMagnetAnalytics interface definition

---
+++ src/lib/leadMagnetAnalytics.ts
@@ -0,0 +1,23 @@
+export interface LeadMagnetAnalytics{
+
+leadMagnetId:string;
+
+title:string;
+
+downloads:number;
+
+emailsCollected:number;
+
+customers:number;
+
+conversionRate:number;
+
+revenue:number;
+
+profit:number;
+
+ltv:number;
+
+cac:number;
+
+}
