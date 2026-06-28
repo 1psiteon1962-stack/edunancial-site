4b802f396fbda0574cec63791e50eb97ceb49cad
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 12:25:27 -0700

    Add SubscriberSegment interface definition

---
+++ src/lib/subscriberSegments.ts
@@ -0,0 +1,21 @@
+export interface SubscriberSegment{
+
+id:string;
+
+name:string;
+
+region:string;
+
+country:string;
+
+language:string;
+
+leadMagnet:string;
+
+minimumLTV:number;
+
+minimumPurchases:number;
+
+active:boolean;
+
+}
