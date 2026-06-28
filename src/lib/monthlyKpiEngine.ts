2f1738e9dc0a514de77901bf45a7564159654fec
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 13:25:30 -0700

    Add MonthlyKPI interface definition

---
+++ src/lib/monthlyKpiEngine.ts
@@ -0,0 +1,25 @@
+export interface MonthlyKPI{
+
+month:string;
+
+users:number;
+
+customers:number;
+
+books:number;
+
+courses:number;
+
+memberships:number;
+
+revenue:number;
+
+expenses:number;
+
+profit:number;
+
+growth:number;
+
+margin:number;
+
+}
