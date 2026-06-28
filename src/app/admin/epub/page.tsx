590ebe9f637dc799e51a8dff6165dad35980b789
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-27 23:33:57 -0700

    Create EPUBManager component for file uploads

---
+++ src/app/admin/epub/page.tsx
@@ -0,0 +1,28 @@
+export default function EPUBManager(){
+
+return(
+
+<main className="min-h-screen bg-[#08101f] text-white p-10">
+
+<h1 className="text-6xl font-black">
+
+EPUB Manager
+
+</h1>
+
+<div
+className="rounded-2xl bg-[#101a2f] p-8 mt-12"
+>
+
+<input
+type="file"
+className="rounded-xl bg-[#08101f] p-4 w-full"
+/>
+
+</div>
+
+</main>
+
+);
+
+}
