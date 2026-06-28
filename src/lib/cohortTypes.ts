733bcc1cd5065247c399d3585a3c725b64d6f205
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 12:59:57 -0700

    Add CustomerCohort interface definition

---
+++ src/lib/cohortTypes.ts
@@ -0,0 +1,21 @@
+export interface CustomerCohort{
+
+id:string;
+
+month:string;
+
+newCustomers:number;
+
+activeCustomers:number;
+
+returningCustomers:number;
+
+retentionRate:number;
+
+averageLTV:number;
+
+averageCAC:number;
+
+revenue:number;
+
+}
