cf6f1fe9ff11807d45f00dfd6099b2cf5ca45fbf
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 12:54:45 -0700

    Add Attribution interface to attributionTypes.ts

---
+++ src/lib/attributionTypes.ts
@@ -0,0 +1,19 @@
+export interface Attribution{
+
+subscriberId:string;
+
+source:string;
+
+medium:string;
+
+campaign:string;
+
+content:string;
+
+keyword:string;
+
+landingPage:string;
+
+created:string;
+
+}
