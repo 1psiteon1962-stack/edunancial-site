3ae2214a0a58b77bc83adbda3de4598b08da0e4c
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 12:15:38 -0700

    Add Certificate interface for type definitions

---
+++ src/lib/certificateTypes.ts
@@ -0,0 +1,17 @@
+export interface Certificate{
+
+id:string;
+
+userId:string;
+
+courseId:string;
+
+studentName:string;
+
+courseName:string;
+
+completedOn:string;
+
+certificateNumber:string;
+
+}
