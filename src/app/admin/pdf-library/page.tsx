e42502cbf368127c483479b47ba8f78c086abec0
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-27 23:33:12 -0700

    Create PdfLibrary component for PDF management

---
+++ src/app/admin/pdf-library/page.tsx
@@ -0,0 +1,29 @@
+export default function PdfLibrary(){
+
+return(
+
+<main className="min-h-screen bg-[#08101f] text-white p-10">
+
+<h1 className="text-6xl font-black">
+
+PDF Library
+
+</h1>
+
+<div
+className="rounded-2xl bg-[#101a2f] p-8 mt-12"
+>
+
+<p>
+
+All uploaded PDF files will appear here.
+
+</p>
+
+</div>
+
+</main>
+
+);
+
+}
