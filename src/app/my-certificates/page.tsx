f4a2ef41934eb18b18d942804c9730d2392c459a
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 12:17:07 -0700

    Create MyCertificates page component

---
+++ src/app/my-certificates/page.tsx
@@ -0,0 +1,27 @@
+export default function MyCertificates(){
+
+return(
+
+<main className="min-h-screen bg-slate-950 text-white p-10">
+
+<h1 className="text-5xl font-black">
+
+My Certificates
+
+</h1>
+
+<div className="mt-10 rounded-xl bg-slate-900 p-8">
+
+<p>
+
+Certificates earned will appear here.
+
+</p>
+
+</div>
+
+</main>
+
+);
+
+}
