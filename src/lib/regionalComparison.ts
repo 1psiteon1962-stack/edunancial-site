c036863ec1ef908154396c327c138f4b5a64bd09
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 13:33:03 -0700

    Add RegionalComparison interface definition

---
+++ src/lib/regionalComparison.ts
@@ -0,0 +1,21 @@
+export interface RegionalComparison{
+
+region:string;
+
+revenue:number;
+
+profit:number;
+
+customers:number;
+
+ltv:number;
+
+cac:number;
+
+margin:number;
+
+growth:number;
+
+rank:number;
+
+}
