fb70ce49588494b2b4669235ba245655994dba2a
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 12:55:21 -0700

    Add CampaignAnalytics interface definition

---
+++ src/lib/campaignAnalytics.ts
@@ -0,0 +1,23 @@
+export interface CampaignAnalytics{
+
+campaignId:string;
+
+impressions:number;
+
+clicks:number;
+
+leads:number;
+
+sales:number;
+
+revenue:number;
+
+cost:number;
+
+cac:number;
+
+roas:number;
+
+ltv:number;
+
+}
