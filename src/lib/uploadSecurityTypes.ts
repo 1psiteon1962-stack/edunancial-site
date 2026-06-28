5085d47ae601a4189753a289246bb3f695cf8167
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 14:07:21 -0700

    Add SecureUpload interface for file uploads

---
+++ src/lib/uploadSecurityTypes.ts
@@ -0,0 +1,19 @@
+export interface SecureUpload{
+
+id:string;
+
+filename:string;
+
+mimeType:string;
+
+size:number;
+
+ownerId:string;
+
+approved:boolean;
+
+quarantined:boolean;
+
+createdAt:string;
+
+}
