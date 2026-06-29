5978d135d8f67aaadefd0ac61d7f185a875999fe
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 16:30:26 -0700

    Add LanguageSelector component for language selection

---
+++ src/components/localization/LanguageSelector.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+export default function LanguageSelector(){
+
+return(
+
+<select className="rounded-lg border p-2">
+
+<option>English</option>
+
+<option>Español</option>
+
+<option>Français</option>
+
+<option>العربية</option>
+
+<option>Português</option>
+
+</select>
+
+);
+
+}
