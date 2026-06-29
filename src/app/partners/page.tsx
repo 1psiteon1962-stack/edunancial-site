d0c0362ccfea5fc6333d52e84dbc20c231a9ebea
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 14:16:02 -0700

    Create PartnersPage component for partners section

---
+++ src/app/partners/page.tsx
@@ -0,0 +1,27 @@
+export default function PartnersPage(){
+
+return(
+
+<main className="min-h-screen bg-slate-100">
+
+<div className="max-w-6xl mx-auto p-10">
+
+<h1 className="text-5xl font-black">
+
+Partners
+
+</h1>
+
+<p className="mt-6">
+
+Organizations working together with Edunancial.
+
+</p>
+
+</div>
+
+</main>
+
+);
+
+}
