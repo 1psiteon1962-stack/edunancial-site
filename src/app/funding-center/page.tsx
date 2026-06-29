879d867e88802a5283710fc42bae283a03a12c3c
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 09:59:03 -0700

    Create FundingCenter component with funding options

---
+++ src/app/funding-center/page.tsx
@@ -0,0 +1,61 @@
+export default function FundingCenter(){
+
+return(
+
+<main className="min-h-screen bg-slate-100">
+
+<div className="max-w-7xl mx-auto p-10">
+
+<h1 className="text-5xl font-bold">
+
+Funding Center
+
+</h1>
+
+<div className="grid lg:grid-cols-3 gap-6 mt-10">
+
+<div className="rounded-xl bg-white shadow p-6">
+
+Loans
+
+</div>
+
+<div className="rounded-xl bg-white shadow p-6">
+
+Grants
+
+</div>
+
+<div className="rounded-xl bg-white shadow p-6">
+
+Crowdfunding
+
+</div>
+
+<div className="rounded-xl bg-white shadow p-6">
+
+Angel Investors
+
+</div>
+
+<div className="rounded-xl bg-white shadow p-6">
+
+Microloans
+
+</div>
+
+<div className="rounded-xl bg-white shadow p-6">
+
+Revenue-Based Financing
+
+</div>
+
+</div>
+
+</div>
+
+</main>
+
+);
+
+}
