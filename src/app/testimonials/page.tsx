97bdec023d5f394e92a9ddddd5a060ce13cf51e4
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 14:41:49 -0700

    Create TestimonialsPage component for success stories

---
+++ src/app/testimonials/page.tsx
@@ -0,0 +1,27 @@
+export default function TestimonialsPage(){
+
+return(
+
+<main className="min-h-screen bg-white">
+
+<div className="max-w-6xl mx-auto p-10">
+
+<h1 className="text-5xl font-black">
+
+Success Stories
+
+</h1>
+
+<p className="mt-6">
+
+Student and entrepreneur success stories will appear here.
+
+</p>
+
+</div>
+
+</main>
+
+);
+
+}
