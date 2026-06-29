598e576465a8e6c150f6601137be2b407a42f009
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 10:35:49 -0700

    Add PlatformStats component for displaying stats

---
+++ src/components/home/PlatformStats.tsx
@@ -0,0 +1,35 @@
+export default function PlatformStats(){
+
+return(
+
+<section className="grid lg:grid-cols-4 gap-6">
+
+<div className="rounded-xl bg-white p-6 shadow">
+
+Courses
+
+</div>
+
+<div className="rounded-xl bg-white p-6 shadow">
+
+Students
+
+</div>
+
+<div className="rounded-xl bg-white p-6 shadow">
+
+Certificates
+
+</div>
+
+<div className="rounded-xl bg-white p-6 shadow">
+
+Professionals
+
+</div>
+
+</section>
+
+);
+
+}
