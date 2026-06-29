d7b19d8279c3c616b1e02508f32d2597dcb62f75
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 10:58:40 -0700

    Create SuccessMetrics component for metrics display

---
+++ src/components/home/SuccessMetrics.tsx
@@ -0,0 +1,35 @@
+export default function SuccessMetrics(){
+
+return(
+
+<section className="grid lg:grid-cols-4 gap-6">
+
+<div className="rounded-xl bg-green-50 p-6">
+
+Revenue
+
+</div>
+
+<div className="rounded-xl bg-blue-50 p-6">
+
+Profit
+
+</div>
+
+<div className="rounded-xl bg-red-50 p-6">
+
+Courses
+
+</div>
+
+<div className="rounded-xl bg-yellow-50 p-6">
+
+Businesses Started
+
+</div>
+
+</section>
+
+);
+
+}
