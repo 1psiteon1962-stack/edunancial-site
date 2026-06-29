1794b4c673823f6402f99adb4b163037ec9bf513
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 23:39:52 -0700

    Create Leaderboard component in page.tsx

---
+++ src/app/leaderboard/page.tsx
@@ -0,0 +1,21 @@
+export default function Leaderboard(){
+
+return(
+
+<main className="min-h-screen bg-white">
+
+<div className="max-w-6xl mx-auto p-10">
+
+<h1 className="text-5xl font-bold">
+
+Leaderboard
+
+</h1>
+
+</div>
+
+</main>
+
+);
+
+}
