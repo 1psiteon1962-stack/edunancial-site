aa274491d2b273728e821143541b2b9b08332e6f
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 18:19:01 -0700

    Add ProviderAgreement interface definition

---
+++ src/lib/providerAgreementTypes.ts
@@ -0,0 +1,19 @@
+export interface ProviderAgreement{
+
+providerId:string;
+
+agreementVersion:string;
+
+accepted:boolean;
+
+acceptedAt:string;
+
+ipAddress:string;
+
+browser:string;
+
+device:string;
+
+electronicSignature:string;
+
+}
