f224448582c27ce4d3a0497ac108fb19f83c9582
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 14:57:01 -0700

    Create Workshops page component

---
+++ src/app/workshops/page.tsx
@@ -0,0 +1,27 @@
+export default function Workshops(){
+
+return(
+
+<main className="min-h-screen bg-white">
+
+<div className="max-w-7xl mx-auto p-10">
+
+<h1 className="text-5xl font-black">
+
+Workshops
+
+</h1>
+
+<p className="mt-6">
+
+Hands-on business and financial workshops.
+
+</p>
+
+</div>
+
+</main>
+
+);
+
+}
