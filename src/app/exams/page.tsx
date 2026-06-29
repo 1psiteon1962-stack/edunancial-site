bb51727e30b2bb08fb47487c15f95ebbc9036912
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 23:37:54 -0700

    Create Exams page component with layout

---
+++ src/app/exams/page.tsx
@@ -0,0 +1,27 @@
+export default function Exams(){
+
+return(
+
+<main className="min-h-screen bg-white">
+
+<div className="max-w-6xl mx-auto p-10">
+
+<h1 className="text-5xl font-bold">
+
+Final Exams
+
+</h1>
+
+<div className="rounded-xl border p-8 mt-8">
+
+Available Exams
+
+</div>
+
+</div>
+
+</main>
+
+);
+
+}
