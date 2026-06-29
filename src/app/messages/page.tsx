ea95d899af82729413015018908b3c362fe4954f
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 23:41:10 -0700

    Create Messages component for displaying messages

---
+++ src/app/messages/page.tsx
@@ -0,0 +1,27 @@
+export default function Messages(){
+
+return(
+
+<main className="min-h-screen bg-white">
+
+<div className="max-w-6xl mx-auto p-10">
+
+<h1 className="text-5xl font-bold">
+
+Messages
+
+</h1>
+
+<div className="rounded-xl border p-8 mt-8">
+
+Inbox
+
+</div>
+
+</div>
+
+</main>
+
+);
+
+}
