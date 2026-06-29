c1e94b87492f6c20a1056c8fd30b50228338f6bd
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 14:22:52 -0700

    Create MediaPage component for press and media

---
+++ src/app/media/page.tsx
@@ -0,0 +1,27 @@
+export default function MediaPage(){
+
+return(
+
+<main className="min-h-screen bg-slate-100">
+
+<div className="max-w-6xl mx-auto p-10">
+
+<h1 className="text-5xl font-black">
+
+Press & Media
+
+</h1>
+
+<p className="mt-6">
+
+News, interviews and media resources.
+
+</p>
+
+</div>
+
+</main>
+
+);
+
+}
