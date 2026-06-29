989a0921bbc74be53379d5e1d0bd90e00e76a2a4
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 14:53:07 -0700

    Create StudentSuccess component for student overview

---
+++ src/app/student-success/page.tsx
@@ -0,0 +1,43 @@
+export default function StudentSuccess(){
+
+return(
+
+<main className="min-h-screen bg-slate-100">
+
+<div className="max-w-7xl mx-auto p-10">
+
+<h1 className="text-5xl font-black">
+
+Student Success
+
+</h1>
+
+<div className="grid lg:grid-cols-3 gap-6 mt-10">
+
+<div className="rounded-xl bg-white shadow p-6">
+
+Completed Courses
+
+</div>
+
+<div className="rounded-xl bg-white shadow p-6">
+
+Certificates
+
+</div>
+
+<div className="rounded-xl bg-white shadow p-6">
+
+Business Success
+
+</div>
+
+</div>
+
+</div>
+
+</main>
+
+);
+
+}
