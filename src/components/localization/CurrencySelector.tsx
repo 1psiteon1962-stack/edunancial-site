c2b8c3c51ea674b6e1800421882acc5df84f2ff5
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 16:30:59 -0700

    Add CurrencySelector component for currency selection

---
+++ src/components/localization/CurrencySelector.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+export default function CurrencySelector(){
+
+return(
+
+<select className="rounded-lg border p-2">
+
+<option>USD</option>
+
+<option>CAD</option>
+
+<option>UGX</option>
+
+<option>KES</option>
+
+<option>NGN</option>
+
+<option>DOP</option>
+
+<option>MXN</option>
+
+</select>
+
+);
+
+}
