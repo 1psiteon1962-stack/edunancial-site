ccefcd982d3d80e4cfca247dc453048702a18acc
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 10:33:27 -0700

    Create UpcomingLiveClasses component

---
+++ src/components/home/UpcomingLiveClasses.tsx
@@ -0,0 +1,23 @@
+export default function UpcomingLiveClasses(){
+
+return(
+
+<section className="rounded-xl bg-white shadow p-8">
+
+<h2 className="text-3xl font-bold">
+
+Upcoming Live Classes
+
+</h2>
+
+<p className="mt-4">
+
+No live classes scheduled.
+
+</p>
+
+</section>
+
+);
+
+}
