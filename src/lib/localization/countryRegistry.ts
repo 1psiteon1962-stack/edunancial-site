28e729ccac95da8069abad805c3c923c1d2698ea
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 16:28:01 -0700

    Add country registry for United States

---
+++ src/lib/localization/countryRegistry.ts
@@ -0,0 +1,35 @@
+import {CountryProfile} from "./countryProfile";
+
+export const CountryRegistry:CountryProfile[]=[
+
+{
+
+country:"United States",
+
+isoCode:"US",
+
+language:["English","Spanish"],
+
+currency:"USD",
+
+currencySymbol:"$",
+
+timezone:"America/New_York",
+
+paymentProviders:["Stripe","PayPal"],
+
+defaultMembership:29.99,
+
+recommendedCoursePrice:149,
+
+marketplaceEnabled:true,
+
+startupHubEnabled:true,
+
+fundingCenterEnabled:true,
+
+voiceAIEnabled:true
+
+}
+
+];
