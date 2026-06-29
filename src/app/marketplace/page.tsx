9feef6589bf8ae9e59816d6ffb6f95ab0f1caa61
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 18:50:47 -0700

    Create Marketplace component for professional services

---
+++ src/app/marketplace/page.tsx
@@ -0,0 +1,61 @@
+export default function Marketplace(){
+
+return(
+
+<main className="min-h-screen bg-white">
+
+<div className="max-w-7xl mx-auto p-10">
+
+<h1 className="text-5xl font-bold">
+
+Professional Marketplace
+
+</h1>
+
+<div className="grid gap-6 lg:grid-cols-3 mt-10">
+
+<div className="border rounded-xl p-6">
+
+Attorney
+
+</div>
+
+<div className="border rounded-xl p-6">
+
+CPA
+
+</div>
+
+<div className="border rounded-xl p-6">
+
+Insurance
+
+</div>
+
+<div className="border rounded-xl p-6">
+
+Bookkeeper
+
+</div>
+
+<div className="border rounded-xl p-6">
+
+Business Consultant
+
+</div>
+
+<div className="border rounded-xl p-6">
+
+Financial Planner
+
+</div>
+
+</div>
+
+</div>
+
+</main>
+
+);
+
+}
