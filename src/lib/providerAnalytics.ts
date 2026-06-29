a43bff009ca5d566c504647f881f4cd0cca5416a
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 18:22:14 -0700

    Add ProviderAnalytics interface definition

---
+++ src/lib/providerAnalytics.ts
@@ -0,0 +1,17 @@
+export interface ProviderAnalytics{
+
+profileViews:number;
+
+aiAppearances:number;
+
+leadRequests:number;
+
+acceptedLeads:number;
+
+conversionRate:number;
+
+monthlyRevenue:number;
+
+customerRating:number;
+
+}
