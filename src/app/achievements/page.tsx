26d9fa1c40ecd3d6498cae327a6dfb23d0813e73
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 23:39:18 -0700

    Create Achievements page component

---
+++ src/app/achievements/page.tsx
@@ -0,0 +1,21 @@
+export default function Achievements(){
+
+return(
+
+<main className="min-h-screen bg-white">
+
+<div className="max-w-6xl mx-auto p-10">
+
+<h1 className="text-5xl font-bold">
+
+Achievements
+
+</h1>
+
+</div>
+
+</main>
+
+);
+
+}
