fda6c8f7f1150a0ac52be04abb31a0772d93ba11
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 11:35:05 -0700

    Add EntrepreneurJourney component

---
+++ src/components/home/EntrepreneurJourney.tsx
@@ -0,0 +1,35 @@
+export default function EntrepreneurJourney(){
+
+return(
+
+<section className="rounded-xl bg-white shadow p-8">
+
+<h2 className="text-3xl font-bold">
+
+Entrepreneur Journey
+
+</h2>
+
+<ol className="mt-6 space-y-3">
+
+<li>Learn</li>
+
+<li>Plan</li>
+
+<li>Launch</li>
+
+<li>Track KPIs</li>
+
+<li>Grow Profit</li>
+
+<li>Seek Capital</li>
+
+<li>Scale</li>
+
+</ol>
+
+</section>
+
+);
+
+}
