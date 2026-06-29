e160330e979f7ccc4609e747beb7d0048e0245eb
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 08:43:38 -0700

    Add FundingOptions component with funding resources info

---
+++ src/app/funding-options/page.tsx
@@ -0,0 +1,27 @@
+export default function FundingOptions(){
+
+return(
+
+<main className="min-h-screen bg-white">
+
+<div className="max-w-6xl mx-auto p-8">
+
+<h1 className="text-5xl font-bold">
+
+Funding Options
+
+</h1>
+
+<p className="mt-6">
+
+Explore grants, loans, microloans, crowdfunding and other educational funding resources.
+
+</p>
+
+</div>
+
+</main>
+
+);
+
+}
