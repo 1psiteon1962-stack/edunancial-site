1b976dcce50cfb54a0df5241c459a4e9c47bc2d8
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 14:15:27 -0700

    Create TeamPage component for team overview
    Added a new TeamPage component to display team members.

---
+++ src/app/team/page.tsx
@@ -0,0 +1,43 @@
+export default function TeamPage(){
+
+return(
+
+<main className="min-h-screen bg-white">
+
+<div className="max-w-7xl mx-auto p-10">
+
+<h1 className="text-5xl font-black">
+
+Meet Our Team
+
+</h1>
+
+<div className="grid lg:grid-cols-3 gap-8 mt-10">
+
+<div className="rounded-xl border p-8">
+
+Founder
+
+</div>
+
+<div className="rounded-xl border p-8">
+
+Leadership
+
+</div>
+
+<div className="rounded-xl border p-8">
+
+Advisors
+
+</div>
+
+</div>
+
+</div>
+
+</main>
+
+);
+
+}
