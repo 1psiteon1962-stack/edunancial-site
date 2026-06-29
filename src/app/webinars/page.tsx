a5cf768393c73524b0f590e9742a5bc8657606d0
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 14:56:00 -0700

    Create Webinars page component

---
+++ src/app/webinars/page.tsx
@@ -0,0 +1,27 @@
+export default function Webinars(){
+
+return(
+
+<main className="min-h-screen bg-white">
+
+<div className="max-w-7xl mx-auto p-10">
+
+<h1 className="text-5xl font-black">
+
+Webinars
+
+</h1>
+
+<p className="mt-6">
+
+Register for upcoming educational webinars.
+
+</p>
+
+</div>
+
+</main>
+
+);
+
+}
