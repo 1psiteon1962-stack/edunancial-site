f8e9a2d12ceb1d49a8764a54b22ae75c924f54a6
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 18:16:27 -0700

    Add ProviderRiskScore interface definition

---
+++ src/lib/providerRiskScore.ts
@@ -0,0 +1,15 @@
+export interface ProviderRiskScore{
+
+paymentHistory:number;
+
+complaints:number;
+
+policyViolations:number;
+
+licenseStatus:number;
+
+insuranceStatus:number;
+
+overallScore:number;
+
+}
