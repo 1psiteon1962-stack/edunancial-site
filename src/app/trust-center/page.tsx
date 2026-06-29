08a2b8ade602e578356de319ccd71434f40e0839
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 14:25:44 -0700

    Create TrustCenter component with layout

---
+++ src/app/trust-center/page.tsx
@@ -0,0 +1,43 @@
+export default function TrustCenter(){
+
+return(
+
+<main className="min-h-screen bg-slate-100">
+
+<div className="max-w-6xl mx-auto p-10">
+
+<h1 className="text-5xl font-black">
+
+Trust Center
+
+</h1>
+
+<div className="grid lg:grid-cols-3 gap-6 mt-10">
+
+<div className="rounded-xl bg-white shadow p-6">
+
+Privacy
+
+</div>
+
+<div className="rounded-xl bg-white shadow p-6">
+
+Security
+
+</div>
+
+<div className="rounded-xl bg-white shadow p-6">
+
+Compliance
+
+</div>
+
+</div>
+
+</div>
+
+</main>
+
+);
+
+}
