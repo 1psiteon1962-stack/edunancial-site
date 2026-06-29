27cb99c8bd0f75a20578fafe5eae720a294af339
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 23:37:18 -0700

    Create Quizzes page component

---
+++ src/app/quizzes/page.tsx
@@ -0,0 +1,27 @@
+export default function Quizzes(){
+
+return(
+
+<main className="min-h-screen bg-white">
+
+<div className="max-w-6xl mx-auto p-10">
+
+<h1 className="text-5xl font-bold">
+
+Course Quizzes
+
+</h1>
+
+<div className="rounded-xl border p-8 mt-8">
+
+Quiz List
+
+</div>
+
+</div>
+
+</main>
+
+);
+
+}
