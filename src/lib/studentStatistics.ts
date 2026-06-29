9a07219bebf569bc1c80d2912ff51c7c3ab0df89
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 06:24:47 -0700

    Add StudentStatistics interface for student data

---
+++ src/lib/studentStatistics.ts
@@ -0,0 +1,15 @@
+export interface StudentStatistics{
+
+hoursStudied:number;
+
+coursesCompleted:number;
+
+booksCompleted:number;
+
+quizzesPassed:number;
+
+certificatesEarned:number;
+
+streak:number;
+
+}
