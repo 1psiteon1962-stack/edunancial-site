4939a3f6b7be74afe6888dda404b769952ad6aed
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 16:29:52 -0700

    Add CountrySelector component with country options

---
+++ src/components/localization/CountrySelector.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+export default function CountrySelector(){
+
+return(
+
+<select className="rounded-lg border p-2">
+
+<option>
+
+United States
+
+</option>
+
+<option>
+
+Canada
+
+</option>
+
+<option>
+
+Uganda
+
+</option>
+
+<option>
+
+Kenya
+
+</option>
+
+<option>
+
+Nigeria
+
+</option>
+
+<option>
+
+Dominican Republic
+
+</option>
+
+<option>
+
+Mexico
+
+</option>
+
+</select>
+
+);
+
+}
