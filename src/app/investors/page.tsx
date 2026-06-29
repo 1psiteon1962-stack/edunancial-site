e32ac1090fd8848693601c58b1db4be934e3b093
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 08:42:38 -0700

    Create Investors page component

---
+++ src/app/investors/page.tsx
@@ -0,0 +1,27 @@
+export default function Investors(){
+
+return(
+
+<main className="min-h-screen bg-white">
+
+<div className="max-w-7xl mx-auto p-8">
+
+<h1 className="text-5xl font-bold">
+
+Investor Center
+
+</h1>
+
+<p className="mt-6">
+
+Review participating startups that have voluntarily chosen to be visible.
+
+</p>
+
+</div>
+
+</main>
+
+);
+
+}
