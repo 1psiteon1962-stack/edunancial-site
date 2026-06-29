5a9acec71ba9b09f8f9da76185e5ba52d9d7e540
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 14:24:41 -0700

    Create AccessibilityPage component for accessibility info

---
+++ src/app/accessibility/page.tsx
@@ -0,0 +1,27 @@
+export default function AccessibilityPage(){
+
+return(
+
+<main className="min-h-screen bg-slate-100">
+
+<div className="max-w-5xl mx-auto p-10">
+
+<h1 className="text-5xl font-black">
+
+Accessibility
+
+</h1>
+
+<p className="mt-8">
+
+Edunancial is committed to making financial education accessible to everyone.
+
+</p>
+
+</div>
+
+</main>
+
+);
+
+}
