359e4bb98bb39bf1f180795ce2c06a9d917a333e
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 14:16:32 -0700

    Create CareersPage component for careers section

---
+++ src/app/careers/page.tsx
@@ -0,0 +1,27 @@
+export default function CareersPage(){
+
+return(
+
+<main className="min-h-screen bg-white">
+
+<div className="max-w-6xl mx-auto p-10">
+
+<h1 className="text-5xl font-black">
+
+Careers
+
+</h1>
+
+<p className="mt-6">
+
+Help us build the future of financial education.
+
+</p>
+
+</div>
+
+</main>
+
+);
+
+}
