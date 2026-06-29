88dd296abfb5bbde3bce7af8998185d50d6e3ca5
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 14:54:46 -0700

    Create CommunityForum component for user interaction

---
+++ src/app/community-forum/page.tsx
@@ -0,0 +1,27 @@
+export default function CommunityForum(){
+
+return(
+
+<main className="min-h-screen bg-white">
+
+<div className="max-w-7xl mx-auto p-10">
+
+<h1 className="text-5xl font-black">
+
+Community Forum
+
+</h1>
+
+<p className="mt-6">
+
+Ask questions, share ideas and learn from other members.
+
+</p>
+
+</div>
+
+</main>
+
+);
+
+}
