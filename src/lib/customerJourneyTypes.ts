2be643e0d398c8df5020855dc1ba202dd685d0e6
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 07:54:26 -0700

    Add CustomerJourney interface definition

---
+++ src/lib/customerJourneyTypes.ts
@@ -0,0 +1,17 @@
+export interface CustomerJourney{
+
+visitorId:string;
+
+landingPage:string;
+
+leadMagnet:string;
+
+emailCaptured:boolean;
+
+purchased:boolean;
+
+ltv:number;
+
+cac:number;
+
+}
