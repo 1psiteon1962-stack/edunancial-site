e8be2882360bfa6167fb7fa1f018dc675867862e
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 06:17:31 -0700

    Add CourseProgress interface for course tracking

---
+++ src/lib/courseProgressTypes.ts
@@ -0,0 +1,17 @@
+export interface CourseProgress{
+
+userId:string;
+
+courseId:string;
+
+completedLessons:number;
+
+totalLessons:number;
+
+percentComplete:number;
+
+lastLesson:string;
+
+certificateEarned:boolean;
+
+}
