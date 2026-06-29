b4dde03400b907dceee53e25ffc8190279728cf4
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 16:29:19 -0700

    Add CountrySuggestion component for localization

---
+++ src/components/localization/CountrySuggestion.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+export default function CountrySuggestion(){
+
+return(
+
+<div className="bg-blue-50 border rounded-xl p-4">
+
+<strong>
+
+Welcome!
+
+</strong>
+
+<p>
+
+We think we know your country.
+
+Would you like to use the localized experience?
+
+</p>
+
+<div className="mt-4 flex gap-3">
+
+<button>
+
+Yes
+
+</button>
+
+<button>
+
+Choose Another Country
+
+</button>
+
+</div>
+
+</div>
+
+);
+
+}
