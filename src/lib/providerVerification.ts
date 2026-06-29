348acdd9fe6d17e6e950f6805bee16966cd6dd0d
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-28 18:36:42 -0700

    Add ProviderVerification interface

---
+++ src/lib/providerVerification.ts
@@ -0,0 +1,15 @@
+export interface ProviderVerification{
+
+identityVerified:boolean;
+
+licenseVerified:boolean;
+
+insuranceVerified:boolean;
+
+emailVerified:boolean;
+
+phoneVerified:boolean;
+
+approved:boolean;
+
+}
