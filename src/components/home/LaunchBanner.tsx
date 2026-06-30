89cd220f2cdd1979dd263c9deead209a07a16af0
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 17:26:48 -0700

    Add LaunchBanner component for North America launch

---
+++ src/components/home/LaunchBanner.tsx
@@ -0,0 +1,25 @@
+export default function LaunchBanner(){
+
+return(
+
+<section className="rounded-xl bg-blue-700 text-white p-8 mt-10">
+
+<h2 className="text-4xl font-black">
+
+North America Launch
+
+</h2>
+
+<p className="mt-4">
+
+Welcome to the first public release of Edunancial.
+
+More countries will be added as they officially launch.
+
+</p>
+
+</section>
+
+);
+
+}
