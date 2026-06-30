4e69eb585416dda1976266610df65e5440c9b10c
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 11:41:37 -0700

    Add FinancialStatementReadiness component

---
+++ src/components/marketplace/FinancialStatementReadiness.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+export default function FinancialStatementReadiness(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Financial Statement Readiness
+
+</h2>
+
+<div className="grid md:grid-cols-2 gap-6 mt-10">
+
+<div className="rounded-xl bg-slate-800 p-6">
+
+Balance Sheet
+
+</div>
+
+<div className="rounded-xl bg-slate-800 p-6">
+
+Profit & Loss
+
+</div>
+
+<div className="rounded-xl bg-slate-800 p-6">
+
+Cash Flow Statement
+
+</div>
+
+<div className="rounded-xl bg-slate-800 p-6">
+
+Supporting Documents
+
+</div>
+
+</div>
+
+</section>
+
+);
+
+}
