9289708cfedf1865214abd1e85e910a4873dd60c
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 17:06:29 -0700

    Add RegionSelector component for region selection

---
+++ src/components/localization/RegionSelector.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+export default function RegionSelector(){
+
+return(
+
+<select className="rounded-lg border p-2">
+
+<option>North America</option>
+
+<option>Latin America</option>
+
+<option>Caribbean</option>
+
+<option>Africa</option>
+
+<option>Middle East</option>
+
+<option>Asia</option>
+
+<option>Europe</option>
+
+</select>
+
+);
+
+}
