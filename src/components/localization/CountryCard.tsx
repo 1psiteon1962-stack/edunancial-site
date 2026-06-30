97f089a294ca01f9fe0cb5d85401163ba43a44df
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 17:07:11 -0700

    Add CountryCard component for country profile display

---
+++ src/components/localization/CountryCard.tsx
@@ -0,0 +1,31 @@
+export default function CountryCard(){
+
+return(
+
+<div className="rounded-xl border shadow p-6">
+
+<h3 className="text-2xl font-bold">
+
+Country Profile
+
+</h3>
+
+<ul className="mt-4 space-y-2">
+
+<li>Language</li>
+
+<li>Currency</li>
+
+<li>Pricing</li>
+
+<li>Payment Methods</li>
+
+<li>Marketplace</li>
+
+</ul>
+
+</div>
+
+);
+
+}
