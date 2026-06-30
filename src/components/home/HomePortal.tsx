e20c7e1cffec320f4764018c3820b64eb90c5d0a
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 18:17:48 -0700

    Add HomePortal component with various sections

---
+++ src/components/home/HomePortal.tsx
@@ -0,0 +1,42 @@
+import FundingCenter from "./FundingCenter";
+import StartupCenter from "./StartupCenter";
+import InvestorCenter from "./InvestorCenter";
+import ExecutiveDashboard from "./ExecutiveDashboard";
+import VoiceAssistant from "./VoiceAssistant";
+import GlobalLaunchRoadmap from "./GlobalLaunchRoadmap";
+import MarketplacePreview from "./MarketplacePreview";
+import CountryAvailability from "./CountryAvailability";
+import UpcomingFeatures from "./UpcomingFeatures";
+import WhoWeArePreview from "./WhoWeArePreview";
+
+export default function HomePortal() {
+
+return(
+
+<>
+
+<VoiceAssistant/>
+
+<FundingCenter/>
+
+<StartupCenter/>
+
+<InvestorCenter/>
+
+<MarketplacePreview/>
+
+<GlobalLaunchRoadmap/>
+
+<CountryAvailability/>
+
+<ExecutiveDashboard/>
+
+<UpcomingFeatures/>
+
+<WhoWeArePreview/>
+
+</>
+
+);
+
+}
