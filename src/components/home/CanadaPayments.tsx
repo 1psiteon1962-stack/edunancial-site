a81a3045b859fede30a6a54cc86227e56334ad20
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 20:11:20 -0700

    Create CanadaPayments component for payment options

---
+++ src/components/home/CanadaPayments.tsx
@@ -0,0 +1,33 @@
+export default function CanadaPayments(){
+
+return(
+
+<section className="py-24">
+
+<div className="max-w-5xl mx-auto px-6">
+
+<h2 className="text-5xl font-black">
+
+Canada Payment Center
+
+</h2>
+
+<p className="mt-6 text-xl text-gray-300">
+
+Canadian payment processing and billing options.
+
+</p>
+
+<div className="mt-10 rounded-xl bg-[#111827] p-8">
+
+Future Canadian payment gateway.
+
+</div>
+
+</div>
+
+</section>
+
+);
+
+}
