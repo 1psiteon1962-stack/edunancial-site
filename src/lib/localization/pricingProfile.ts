9290386c57cf04a2541a6fad6afea097c28f9174
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 17:02:34 -0700

    Create PricingProfile interface for localization

---
+++ src/lib/localization/pricingProfile.ts
@@ -0,0 +1,17 @@
+export interface PricingProfile{
+
+country:string;
+
+monthlyMembership:number;
+
+annualMembership:number;
+
+microCoursePrice:number;
+
+flagshipCoursePrice:number;
+
+bookPrice:number;
+
+audioBookPrice:number;
+
+}
