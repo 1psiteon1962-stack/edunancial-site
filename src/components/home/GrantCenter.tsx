c6d5316222e9f6879239dfbbf9c19ae752a9ff92
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 20:33:27 -0700

    Add GrantCenter component for displaying grants

---
+++ src/components/home/GrantCenter.tsx
@@ -0,0 +1,49 @@
+export default function GrantCenter(){
+
+return(
+
+<section className="py-24">
+
+<div className="max-w-6xl mx-auto px-6">
+
+<h2 className="text-5xl font-black">
+
+Grant Center
+
+</h2>
+
+<div className="grid gap-6 mt-12">
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Federal Grants
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+State Grants
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Education Grants
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+International Grants
+
+</div>
+
+</div>
+
+</div>
+
+</section>
+
+);
+
+}
