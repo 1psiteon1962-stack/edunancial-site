ec8e371be0a3936aab0f3fe6d2f9d07385fdaaf9
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 06:52:39 -0700

    Add CourseCompletionStats component

---
+++ src/components/course-evaluation/CourseCompletionStats.tsx
@@ -0,0 +1,23 @@
+export default function CourseCompletionStats() {
+  return (
+    <div className="rounded-xl bg-slate-900 p-6">
+
+      <h3 className="text-xl font-bold">
+        Completion Statistics
+      </h3>
+
+      <ul className="mt-4 space-y-2">
+
+        <li>Total Enrolled</li>
+
+        <li>Total Completed</li>
+
+        <li>Certificates Issued</li>
+
+        <li>Average Completion Time</li>
+
+      </ul>
+
+    </div>
+  );
+}
