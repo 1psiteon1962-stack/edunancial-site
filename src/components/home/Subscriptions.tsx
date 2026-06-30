85912d584c12a2441df7081014768ef8bf64a308
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 20:14:09 -0700

    Create Subscriptions component for billing options

---
+++ src/components/home/Subscriptions.tsx
@@ -0,0 +1,43 @@
+export default function Subscriptions(){
+
+return(
+
+<section className="py-24">
+
+<div className="max-w-6xl mx-auto px-6">
+
+<h2 className="text-5xl font-black">
+
+Subscription Billing
+
+</h2>
+
+<div className="grid md:grid-cols-3 gap-8 mt-12">
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Monthly
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Annual
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Scholarship Codes
+
+</div>
+
+</div>
+
+</div>
+
+</section>
+
+);
+
+}
