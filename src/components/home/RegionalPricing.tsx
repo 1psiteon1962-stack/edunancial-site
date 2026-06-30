7b7195fb2a9fcf64e5aa784c7abdb68fa3816453
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 20:03:19 -0700

    Create RegionalPricing component for pricing display

---
+++ src/components/home/RegionalPricing.tsx
@@ -0,0 +1,49 @@
+export default function RegionalPricing(){
+
+return(
+
+<section className="py-24">
+
+<div className="max-w-6xl mx-auto px-6">
+
+<h2 className="text-5xl font-black">
+
+Regional Pricing
+
+</h2>
+
+<div className="grid md:grid-cols-2 gap-8 mt-12">
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+North America Pricing
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Africa Pricing
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Latin America Pricing
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Europe Pricing
+
+</div>
+
+</div>
+
+</div>
+
+</section>
+
+);
+
+}
