c1db6ca85f4048433a98b86204c29b2b297bdeb4
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 20:35:18 -0700

    Create MicroloanCenter component for loan options

---
+++ src/components/home/MicroloanCenter.tsx
@@ -0,0 +1,49 @@
+export default function MicroloanCenter(){
+
+return(
+
+<section className="py-24">
+
+<div className="max-w-6xl mx-auto px-6">
+
+<h2 className="text-5xl font-black">
+
+Microloan Center
+
+</h2>
+
+<div className="grid md:grid-cols-2 gap-8 mt-12">
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Loans Under $500
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Loans Under $2,000
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Loans Under $5,000
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Business Startup Loans
+
+</div>
+
+</div>
+
+</div>
+
+</section>
+
+);
+
+}
