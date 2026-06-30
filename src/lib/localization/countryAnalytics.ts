4db41291e4f28e7bde7f00c238ab1e6713c7ff4b
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 17:04:49 -0700

    Add CountryAnalytics interface for analytics data

---
+++ src/lib/localization/countryAnalytics.ts
@@ -0,0 +1,19 @@
+export interface CountryAnalytics{
+
+country:string;
+
+users:number;
+
+revenue:number;
+
+memberships:number;
+
+coursesSold:number;
+
+completionRate:number;
+
+averageOrderValue:number;
+
+customerLifetimeValue:number;
+
+}
