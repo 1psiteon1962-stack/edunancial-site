6426d9d67bcff2b91b1adf81a83d7515240da084
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 17:29:56 -0700

    Create HomeEnhancements component with subcomponents

---
+++ src/components/home/HomeEnhancements.tsx
@@ -0,0 +1,24 @@
+import LaunchBanner from "./LaunchBanner";
+import HomeQuickAccess from "./HomeQuickAccess";
+import ComingSoonCountries from "./ComingSoonCountries";
+import FeatureHighlights from "./FeatureHighlights";
+
+export default function HomeEnhancements(){
+
+return(
+
+<>
+
+<LaunchBanner/>
+
+<HomeQuickAccess/>
+
+<FeatureHighlights/>
+
+<ComingSoonCountries/>
+
+</>
+
+);
+
+}
