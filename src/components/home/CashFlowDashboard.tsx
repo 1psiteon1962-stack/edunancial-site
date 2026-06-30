118d88229c0bc87782e0f94e975fb1f123b8c38e
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 20:29:36 -0700

    Create CashFlowDashboard component

---
+++ src/components/home/CashFlowDashboard.tsx
@@ -0,0 +1,43 @@
+export default function CashFlowDashboard(){
+
+return(
+
+<section className="py-24">
+
+<div className="max-w-6xl mx-auto px-6">
+
+<h2 className="text-5xl font-black">
+
+Cash Flow
+
+</h2>
+
+<div className="grid md:grid-cols-3 gap-8 mt-12">
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Income
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Expenses
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Net Cash Flow
+
+</div>
+
+</div>
+
+</div>
+
+</section>
+
+);
+
+}
