ef2e2299ba2f14db5b9fccb9e18b7e5328edc468
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 20:18:57 -0700

    Create ProfileDashboard component for user profile

---
+++ src/components/home/ProfileDashboard.tsx
@@ -0,0 +1,49 @@
+export default function ProfileDashboard(){
+
+return(
+
+<section className="py-24">
+
+<div className="max-w-6xl mx-auto px-6">
+
+<h2 className="text-5xl font-black">
+
+My Profile
+
+</h2>
+
+<div className="grid md:grid-cols-2 gap-8 mt-12">
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Personal Information
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Membership Status
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Certificates
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Downloads
+
+</div>
+
+</div>
+
+</div>
+
+</section>
+
+);
+
+}
