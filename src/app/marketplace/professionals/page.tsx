60bedb0398b4633161911ae4f452668be897e509
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 11:50:36 -0700

    Create ProfessionalsPage with metadata and directory

---
+++ src/app/marketplace/professionals/page.tsx
@@ -0,0 +1,11 @@
+import ProfessionalDirectory from "@/components/marketplace/ProfessionalDirectory";
+
+export const metadata = {
+  title: "Professional Directory | Edunancial Marketplace",
+  description:
+    "Find verified professionals within the Edunancial Marketplace.",
+};
+
+export default function ProfessionalsPage() {
+  return <ProfessionalDirectory />;
+}
