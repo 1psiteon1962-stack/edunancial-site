f170b52450d5aa31bd285ca857432b3cb4cbdc56
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 20:36:02 -0700

    Add BusinessLoanCenter component for loan services

---
+++ src/components/home/BusinessLoanCenter.tsx
@@ -0,0 +1,55 @@
+export default function BusinessLoanCenter(){
+
+return(
+
+<section className="py-24 bg-[#08101f]">
+
+<div className="max-w-6xl mx-auto px-6">
+
+<h2 className="text-5xl font-black">
+
+Business Loan Center
+
+</h2>
+
+<div className="grid gap-6 mt-12">
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+SBA Loans
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Commercial Loans
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Equipment Financing
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Working Capital
+
+</div>
+
+<div className="rounded-xl bg-[#111827] p-8">
+
+Expansion Financing
+
+</div>
+
+</div>
+
+</div>
+
+</section>
+
+);
+
+}
