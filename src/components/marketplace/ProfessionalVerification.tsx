20bb7b868694c5df479fb62310c29478bf2fed02
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 11:37:38 -0700

    Add ProfessionalVerification component

---
+++ src/components/marketplace/ProfessionalVerification.tsx
@@ -0,0 +1,31 @@
+export default function ProfessionalVerification() {
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-3xl font-black">
+
+Professional Verification
+
+</h2>
+
+<ul className="mt-8 space-y-4">
+
+<li>Government Identification</li>
+
+<li>Professional License</li>
+
+<li>Insurance Verification</li>
+
+<li>Business Registration</li>
+
+<li>Background Review</li>
+
+</ul>
+
+</section>
+
+);
+
+}
