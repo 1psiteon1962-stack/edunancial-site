dbbdaa9d6d0de714c8e7668efee74cbd659ad33e
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-29 18:38:45 -0700

    Refactor HomePortal component to simplify imports

--- src/components/home/HomePortal.tsx
+++ src/components/home/HomePortal.tsx
@@ -1,41 +1,20 @@
-import FundingCenter from "./FundingCenter";
-import StartupCenter from "./StartupCenter";
-import InvestorCenter from "./InvestorCenter";
-import ExecutiveDashboard from "./ExecutiveDashboard";
-import VoiceAssistant from "./VoiceAssistant";
 import GlobalLaunchRoadmap from "./GlobalLaunchRoadmap";
 import MarketplacePreview from "./MarketplacePreview";
-import CountryAvailability from "./CountryAvailability";
 import UpcomingFeatures from "./UpcomingFeatures";
-import WhoWeArePreview from "./WhoWeArePreview";
 
 export default function HomePortal() {
 
 return(
 
-<>
-
-<VoiceAssistant/>
-
-<FundingCenter/>
-
-<StartupCenter/>
-
-<InvestorCenter/>
-
-<MarketplacePreview/>
+<section className="bg-[#09111f]">
 
 <GlobalLaunchRoadmap/>
 
-<CountryAvailability/>
-
-<ExecutiveDashboard/>
+<MarketplacePreview/>
 
 <UpcomingFeatures/>
 
-<WhoWeArePreview/>
-
-</>
+</section>
 
 );
 
