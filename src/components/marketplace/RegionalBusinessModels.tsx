41558512be1e456da28e5b99816c47377cb878d7
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:32:09 -0700

    Add RegionalBusinessModels component

---
+++ src/components/marketplace/RegionalBusinessModels.tsx
@@ -0,0 +1,39 @@
+export default function RegionalBusinessModels(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Regional Business Models
+
+</h2>
+
+<div className="grid md:grid-cols-3 gap-6 mt-10">
+
+<div>North America</div>
+
+<div>Latin America</div>
+
+<div>Europe</div>
+
+<div>East Africa</div>
+
+<div>West Africa</div>
+
+<div>Southern Africa</div>
+
+<div>Middle East</div>
+
+<div>Asia</div>
+
+<div>Oceania</div>
+
+</div>
+
+</section>
+
+);
+
+}
