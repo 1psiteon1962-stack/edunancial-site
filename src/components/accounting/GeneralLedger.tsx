821888a60e885b81ce404b418c5979723842328e
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 18:31:48 -0700

    Create GeneralLedger component for accounting

---
+++ src/components/accounting/GeneralLedger.tsx
@@ -0,0 +1,28 @@
+export default function GeneralLedger(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+General Ledger
+
+</h2>
+
+<div className="grid grid-cols-6 gap-4 mt-8 font-bold">
+
+<div>Date</div>
+<div>Account</div>
+<div>Description</div>
+<div>Debit</div>
+<div>Credit</div>
+<div>Balance</div>
+
+</div>
+
+</section>
+
+);
+
+}
