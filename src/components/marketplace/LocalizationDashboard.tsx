8b1afa717a6d1a027e35bf05ca5c407c00812a5c
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:26:37 -0700

    Add LocalizationDashboard component

---
+++ src/components/marketplace/LocalizationDashboard.tsx
@@ -0,0 +1,37 @@
+export default function LocalizationDashboard(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Localization Dashboard
+
+</h2>
+
+<div className="grid md:grid-cols-4 gap-6 mt-10">
+
+<div>Language</div>
+
+<div>Currency</div>
+
+<div>Pricing</div>
+
+<div>Payment Methods</div>
+
+<div>Culture</div>
+
+<div>Case Studies</div>
+
+<div>Examples</div>
+
+<div>Business Environment</div>
+
+</div>
+
+</section>
+
+);
+
+}
