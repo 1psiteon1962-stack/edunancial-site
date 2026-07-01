a339c4195e91449a419b72ae87b0d9ea347af371
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:27:08 -0700

    Add Country Launch Checklist component

---
+++ src/components/marketplace/CountryLaunchChecklist.tsx
@@ -0,0 +1,41 @@
+export default function CountryLaunchChecklist(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Country Launch Checklist
+
+</h2>
+
+<ul className="space-y-3 mt-8">
+
+<li>Website Translation</li>
+
+<li>Currency Configured</li>
+
+<li>Regional Pricing</li>
+
+<li>Payment Provider Connected</li>
+
+<li>Marketplace Professionals Added</li>
+
+<li>Legal Review Completed</li>
+
+<li>SEO Completed</li>
+
+<li>Google Business Profile</li>
+
+<li>Support Staff Assigned</li>
+
+<li>Launch Approved</li>
+
+</ul>
+
+</section>
+
+);
+
+}
