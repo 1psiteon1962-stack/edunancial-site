8e2da2f972f89b46094e0d70186f2fa8b458a547
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:30:11 -0700

    Create PurchasingPowerIndex component

---
+++ src/components/marketplace/PurchasingPowerIndex.tsx
@@ -0,0 +1,31 @@
+export default function PurchasingPowerIndex(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Purchasing Power Index
+
+</h2>
+
+<div className="grid md:grid-cols-5 gap-6 mt-10">
+
+<div>Country</div>
+
+<div>Local Currency</div>
+
+<div>Income Index</div>
+
+<div>Suggested Pricing</div>
+
+<div>Status</div>
+
+</div>
+
+</section>
+
+);
+
+}
