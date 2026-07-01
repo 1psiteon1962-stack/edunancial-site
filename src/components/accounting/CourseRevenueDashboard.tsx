9e71732e6edebb58bfb6f23ce7bf0638651423bc
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 19:00:49 -0700

    Create CourseRevenueDashboard component

---
+++ src/components/accounting/CourseRevenueDashboard.tsx
@@ -0,0 +1,37 @@
+export default function CourseRevenueDashboard(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Course Revenue
+
+</h2>
+
+<div className="grid md:grid-cols-4 gap-6 mt-8">
+
+<div>Course Name</div>
+
+<div>Students</div>
+
+<div>Revenue</div>
+
+<div>Completion %</div>
+
+<div>Refunds</div>
+
+<div>Average Rating</div>
+
+<div>Country</div>
+
+<div>Profit Margin</div>
+
+</div>
+
+</section>
+
+);
+
+}
