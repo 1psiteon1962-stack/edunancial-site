7fe79cf307d4e2d917b367237ecd1106d167e2a9
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:23:13 -0700

    Add CountryLanguages component to display languages

---
+++ src/components/marketplace/CountryLanguages.tsx
@@ -0,0 +1,39 @@
+export default function CountryLanguages(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Supported Languages
+
+</h2>
+
+<div className="grid md:grid-cols-3 gap-6 mt-8">
+
+<div>English</div>
+
+<div>Spanish</div>
+
+<div>French</div>
+
+<div>Portuguese</div>
+
+<div>Swahili</div>
+
+<div>Luganda</div>
+
+<div>German</div>
+
+<div>Italian</div>
+
+<div>Arabic</div>
+
+</div>
+
+</section>
+
+);
+
+}
