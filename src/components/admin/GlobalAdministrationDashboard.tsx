627931e576e922a10cf589260cd0c6780d1fc11e
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 19:32:18 -0700

    Create GlobalAdministrationDashboard component

---
+++ src/components/admin/GlobalAdministrationDashboard.tsx
@@ -0,0 +1,41 @@
+export default function GlobalAdministrationDashboard(){
+
+return(
+
+<main className="min-h-screen bg-[#08101f] text-white">
+
+<section className="max-w-7xl mx-auto px-6 py-16">
+
+<h1 className="text-5xl font-black">
+
+Global Administration
+
+</h1>
+
+<div className="grid lg:grid-cols-4 md:grid-cols-2 gap-6 mt-12">
+
+<div>Regions</div>
+
+<div>Countries</div>
+
+<div>Administrators</div>
+
+<div>Deployments</div>
+
+<div>Marketplace</div>
+
+<div>Courses</div>
+
+<div>Users</div>
+
+<div>System Health</div>
+
+</div>
+
+</section>
+
+</main>
+
+);
+
+}
