adbe4202455d87a20bf1eab85c82fe212e8b7b43
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:20:00 -0700

    Create CountryProfile component for marketplace

---
+++ src/components/marketplace/CountryProfile.tsx
@@ -0,0 +1,45 @@
+export default function CountryProfile(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Country Profile
+
+</h2>
+
+<div className="grid md:grid-cols-2 gap-6 mt-10">
+
+<div>Languages</div>
+
+<div>Currency</div>
+
+<div>Population</div>
+
+<div>Internet Access</div>
+
+<div>Smartphone Usage</div>
+
+<div>Desktop Usage</div>
+
+<div>Average Income</div>
+
+<div>Cost of Living</div>
+
+<div>Business Climate</div>
+
+<div>Housing Market</div>
+
+<div>Common Financing</div>
+
+<div>Marketplace Professionals</div>
+
+</div>
+
+</section>
+
+);
+
+}
