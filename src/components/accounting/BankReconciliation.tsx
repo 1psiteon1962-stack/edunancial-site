8bb5578e301efaa5452aa0ac9fb0f2dc1c957950
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 18:34:21 -0700

    Add BankReconciliation component

---
+++ src/components/accounting/BankReconciliation.tsx
@@ -0,0 +1,29 @@
+export default function BankReconciliation(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Bank Reconciliation
+
+</h2>
+
+<div className="grid md:grid-cols-4 gap-6 mt-8">
+
+<div>Bank Balance</div>
+
+<div>Book Balance</div>
+
+<div>Outstanding Items</div>
+
+<div>Difference</div>
+
+</div>
+
+</section>
+
+);
+
+}
