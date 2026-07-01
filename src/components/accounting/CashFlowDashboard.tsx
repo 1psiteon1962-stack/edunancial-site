745ce97b4eaba3e073d107343496244b069f77e4
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 18:20:09 -0700

    Create CashFlowDashboard component

---
+++ src/components/accounting/CashFlowDashboard.tsx
@@ -0,0 +1,33 @@
+export default function CashFlowDashboard(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Cash Flow Statement
+
+</h2>
+
+<div className="grid md:grid-cols-3 gap-6 mt-10">
+
+<div>Operating Activities</div>
+
+<div>Investing Activities</div>
+
+<div>Financing Activities</div>
+
+<div>Beginning Cash</div>
+
+<div>Ending Cash</div>
+
+<div>Cash Trend</div>
+
+</div>
+
+</section>
+
+);
+
+}
