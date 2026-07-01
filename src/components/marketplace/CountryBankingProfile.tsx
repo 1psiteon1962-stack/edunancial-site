93d42ca87401a207e89ada6b9bd0d85b06637e12
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:40:58 -0700

    Create CountryBankingProfile component

---
+++ src/components/marketplace/CountryBankingProfile.tsx
@@ -0,0 +1,39 @@
+export default function CountryBankingProfile(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Banking Profile
+
+</h2>
+
+<div className="grid md:grid-cols-3 gap-6 mt-10">
+
+<div>Major Banks</div>
+
+<div>Business Accounts</div>
+
+<div>Personal Accounts</div>
+
+<div>Merchant Accounts</div>
+
+<div>Digital Banking</div>
+
+<div>Mobile Banking</div>
+
+<div>International Transfers</div>
+
+<div>Foreign Currency</div>
+
+<div>Business Lending</div>
+
+</div>
+
+</section>
+
+);
+
+}
