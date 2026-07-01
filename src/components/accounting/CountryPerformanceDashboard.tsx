74128f9858573206f8955eea6e9ec6ea1f9cbc7b
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 19:05:05 -0700

    Add CountryPerformanceDashboard component

---
+++ src/components/accounting/CountryPerformanceDashboard.tsx
@@ -0,0 +1,31 @@
+export default function CountryPerformanceDashboard(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Country Performance
+
+</h2>
+
+<div className="grid md:grid-cols-5 gap-6 mt-8">
+
+<div>Country</div>
+
+<div>Revenue</div>
+
+<div>Members</div>
+
+<div>Profit</div>
+
+<div>Growth</div>
+
+</div>
+
+</section>
+
+);
+
+}
