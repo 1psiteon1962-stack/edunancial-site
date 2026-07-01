2f0874d80d89e4983cb7c06805bad7f963e78c15
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:31:30 -0700

    Add CountryCourseCatalog component

---
+++ src/components/marketplace/CountryCourseCatalog.tsx
@@ -0,0 +1,33 @@
+export default function CountryCourseCatalog(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Country Course Catalog
+
+</h2>
+
+<div className="grid md:grid-cols-2 gap-6 mt-10">
+
+<div>Required Courses</div>
+
+<div>Recommended Courses</div>
+
+<div>Localized Courses</div>
+
+<div>Country Case Studies</div>
+
+<div>Translated Courses</div>
+
+<div>Coming Soon</div>
+
+</div>
+
+</section>
+
+);
+
+}
