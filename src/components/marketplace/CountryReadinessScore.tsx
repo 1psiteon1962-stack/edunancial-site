8789c8eaa9e612b4ce43c78f20bcff3fbc5fe229
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:27:40 -0700

    Add CountryReadinessScore component

---
+++ src/components/marketplace/CountryReadinessScore.tsx
@@ -0,0 +1,31 @@
+export default function CountryReadinessScore(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Country Readiness Score
+
+</h2>
+
+<div className="grid md:grid-cols-5 gap-6 mt-10">
+
+<div>Education</div>
+
+<div>Localization</div>
+
+<div>Marketplace</div>
+
+<div>Payments</div>
+
+<div>Overall Score</div>
+
+</div>
+
+</section>
+
+);
+
+}
