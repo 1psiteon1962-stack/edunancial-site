7b28dea348b58ca4e45a3494907e7023e4dbb5ac
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:21:08 -0700

    Add CountryPaymentMethods component

---
+++ src/components/marketplace/CountryPaymentMethods.tsx
@@ -0,0 +1,37 @@
+export default function CountryPaymentMethods(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Payment Methods
+
+</h2>
+
+<ul className="space-y-4 mt-8">
+
+<li>Credit Cards</li>
+
+<li>Debit Cards</li>
+
+<li>PayPal</li>
+
+<li>Mobile Money</li>
+
+<li>Bank Transfer</li>
+
+<li>Apple Pay</li>
+
+<li>Google Pay</li>
+
+<li>Regional Payment Providers</li>
+
+</ul>
+
+</section>
+
+);
+
+}
