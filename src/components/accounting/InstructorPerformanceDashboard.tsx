e95858203de6182d008b59f89792da117e641440
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 19:07:44 -0700

    Create InstructorPerformanceDashboard component

---
+++ src/components/accounting/InstructorPerformanceDashboard.tsx
@@ -0,0 +1,37 @@
+export default function InstructorPerformanceDashboard(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Instructor Performance
+
+</h2>
+
+<div className="grid md:grid-cols-4 gap-6 mt-8">
+
+<div>Instructor</div>
+
+<div>Students</div>
+
+<div>Completion Rate</div>
+
+<div>Average Rating</div>
+
+<div>Revenue</div>
+
+<div>Courses</div>
+
+<div>Certificates</div>
+
+<div>Trend</div>
+
+</div>
+
+</section>
+
+);
+
+}
