5b4b17f7a24c87a3c814c8e77307e5e00b243764
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 19:01:38 -0700

    Create BookRevenueDashboard component

---
+++ src/components/accounting/BookRevenueDashboard.tsx
@@ -0,0 +1,37 @@
+export default function BookRevenueDashboard(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Book Revenue
+
+</h2>
+
+<div className="grid md:grid-cols-4 gap-6 mt-8">
+
+<div>Book</div>
+
+<div>Sales</div>
+
+<div>Revenue</div>
+
+<div>Profit</div>
+
+<div>Country</div>
+
+<div>Language</div>
+
+<div>Rating</div>
+
+<div>Returns</div>
+
+</div>
+
+</section>
+
+);
+
+}
