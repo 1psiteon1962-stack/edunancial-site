21da2cbf7805c1e4486600d26ab2cdcccbd126b8
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:33:51 -0700

    Create CountryBusinessProfile component

---
+++ src/components/marketplace/CountryBusinessProfile.tsx
@@ -0,0 +1,39 @@
+export default function CountryBusinessProfile(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Country Business Profile
+
+</h2>
+
+<div className="grid md:grid-cols-3 gap-6 mt-10">
+
+<div>Major Industries</div>
+
+<div>Average Startup Cost</div>
+
+<div>Small Business Climate</div>
+
+<div>Common Business Types</div>
+
+<div>Typical Profit Margins</div>
+
+<div>Business Failure Rate</div>
+
+<div>Average Business Size</div>
+
+<div>Growth Industries</div>
+
+<div>Economic Outlook</div>
+
+</div>
+
+</section>
+
+);
+
+}
