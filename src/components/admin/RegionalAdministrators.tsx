6dd027f22d9d4d82a6b66b28b4bfc5bddb561fd5
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 19:34:05 -0700

    Create RegionalAdministrators component

---
+++ src/components/admin/RegionalAdministrators.tsx
@@ -0,0 +1,29 @@
+export default function RegionalAdministrators(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Regional Administrators
+
+</h2>
+
+<div className="grid md:grid-cols-4 gap-6 mt-8">
+
+<div>Region</div>
+
+<div>Administrator</div>
+
+<div>Permission Level</div>
+
+<div>Status</div>
+
+</div>
+
+</section>
+
+);
+
+}
