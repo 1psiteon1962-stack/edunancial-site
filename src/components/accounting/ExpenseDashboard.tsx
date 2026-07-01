bdec21f29d8d83cdbe6179d3cadecd008ed5cca5
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 18:49:44 -0700

    Create ExpenseDashboard component for expense overview

---
+++ src/components/accounting/ExpenseDashboard.tsx
@@ -0,0 +1,37 @@
+export default function ExpenseDashboard(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Expense Dashboard
+
+</h2>
+
+<div className="grid md:grid-cols-4 gap-6 mt-10">
+
+<div>Payroll</div>
+
+<div>Marketing</div>
+
+<div>Software</div>
+
+<div>Hosting</div>
+
+<div>Professional Fees</div>
+
+<div>Insurance</div>
+
+<div>Taxes</div>
+
+<div>Other Expenses</div>
+
+</div>
+
+</section>
+
+);
+
+}
