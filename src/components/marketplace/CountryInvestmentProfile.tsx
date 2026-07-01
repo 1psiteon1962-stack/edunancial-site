d4a61983491da69cc3e61c1b5d75e18f5f5ef89b
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:41:34 -0700

    Create CountryInvestmentProfile component

---
+++ src/components/marketplace/CountryInvestmentProfile.tsx
@@ -0,0 +1,39 @@
+export default function CountryInvestmentProfile(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Investment Environment
+
+</h2>
+
+<div className="grid md:grid-cols-3 gap-6 mt-10">
+
+<div>Stock Market</div>
+
+<div>Government Bonds</div>
+
+<div>Real Estate</div>
+
+<div>Private Equity</div>
+
+<div>Angel Investors</div>
+
+<div>Venture Capital</div>
+
+<div>Crowdfunding</div>
+
+<div>Retirement Accounts</div>
+
+<div>Precious Metals</div>
+
+</div>
+
+</section>
+
+);
+
+}
