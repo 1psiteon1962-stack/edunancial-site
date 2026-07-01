b59f22543a2414fa535d20a2beddcd4f73ee1ade
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 19:04:30 -0700

    Add RegionalPerformanceDashboard component

---
+++ src/components/accounting/RegionalPerformanceDashboard.tsx
@@ -0,0 +1,37 @@
+export default function RegionalPerformanceDashboard(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Regional Performance
+
+</h2>
+
+<div className="grid md:grid-cols-4 gap-6 mt-8">
+
+<div>North America</div>
+
+<div>Caribbean</div>
+
+<div>Latin America</div>
+
+<div>Europe</div>
+
+<div>East Africa</div>
+
+<div>West Africa</div>
+
+<div>Southern Africa</div>
+
+<div>Asia-Pacific</div>
+
+</div>
+
+</section>
+
+);
+
+}
