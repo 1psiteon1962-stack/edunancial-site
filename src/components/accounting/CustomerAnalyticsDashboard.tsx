fae2365cf8d3544d83d24b54e59e09fe440068ff
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 19:00:14 -0700

    Add CustomerAnalyticsDashboard component

---
+++ src/components/accounting/CustomerAnalyticsDashboard.tsx
@@ -0,0 +1,37 @@
+export default function CustomerAnalyticsDashboard(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Customer Analytics
+
+</h2>
+
+<div className="grid md:grid-cols-4 gap-6 mt-8">
+
+<div>Total Customers</div>
+
+<div>New Customers</div>
+
+<div>Returning Customers</div>
+
+<div>Retention Rate</div>
+
+<div>Lifetime Value</div>
+
+<div>Average Purchase</div>
+
+<div>Countries Served</div>
+
+<div>Growth Trend</div>
+
+</div>
+
+</section>
+
+);
+
+}
