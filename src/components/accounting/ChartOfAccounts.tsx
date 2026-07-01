a3685134a09b50f5acf93a99ac401ba2d481064f
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 18:19:11 -0700

    Create ChartOfAccounts component for accounting section

---
+++ src/components/accounting/ChartOfAccounts.tsx
@@ -0,0 +1,37 @@
+export default function ChartOfAccounts(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Chart of Accounts
+
+</h2>
+
+<div className="grid md:grid-cols-4 gap-6 mt-10">
+
+<div>Assets</div>
+
+<div>Liabilities</div>
+
+<div>Equity</div>
+
+<div>Revenue</div>
+
+<div>Expenses</div>
+
+<div>Cost of Sales</div>
+
+<div>Other Income</div>
+
+<div>Other Expenses</div>
+
+</div>
+
+</section>
+
+);
+
+}
