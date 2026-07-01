8a452bf0ebafa435cc2fb3c3b872c0c23c4c3985
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 18:20:57 -0700

    Create ProfitLossDashboard component

---
+++ src/components/accounting/ProfitLossDashboard.tsx
@@ -0,0 +1,33 @@
+export default function ProfitLossDashboard(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Profit & Loss Statement
+
+</h2>
+
+<div className="grid md:grid-cols-2 gap-6 mt-10">
+
+<div>Revenue</div>
+
+<div>Cost of Goods Sold</div>
+
+<div>Gross Profit</div>
+
+<div>Operating Expenses</div>
+
+<div>Operating Income</div>
+
+<div>Net Profit</div>
+
+</div>
+
+</section>
+
+);
+
+}
