fd1694655dd926dd3611462848c0367657f40b3a
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:48:16 -0700

    Add OpenGraph metadata for Edunancial

---
+++ src/components/seo/OpenGraphMetadata.tsx
@@ -0,0 +1,33 @@
+export const openGraphMetadata={
+
+title:"Edunancial | Global Financial Literacy Platform",
+
+description:
+
+"Financial Literacy. Business Education. Real Estate. Investor Readiness.",
+
+url:"https://www.edunancial.com",
+
+siteName:"Edunancial",
+
+images:[
+
+{
+
+url:"/images/og-home.jpg",
+
+width:1200,
+
+height:630,
+
+alt:"Edunancial"
+
+}
+
+],
+
+locale:"en_US",
+
+type:"website"
+
+};
