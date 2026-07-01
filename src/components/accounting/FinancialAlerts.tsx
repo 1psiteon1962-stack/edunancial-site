035d7f3dfeca9369230c02f76abfc95f03f69c16
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 19:27:26 -0700

    Create FinancialAlerts component for financial warnings

---
+++ src/components/accounting/FinancialAlerts.tsx
@@ -0,0 +1,33 @@
+export default function FinancialAlerts(){
+
+return(
+
+<section className="rounded-xl bg-red-950 border border-red-700 p-8">
+
+<h2 className="text-4xl font-black">
+
+Financial Alerts
+
+</h2>
+
+<div className="space-y-4 mt-8">
+
+<div>Cash Flow Warning</div>
+
+<div>Revenue Decline</div>
+
+<div>High Expense Alert</div>
+
+<div>Membership Churn</div>
+
+<div>Marketplace Revenue Drop</div>
+
+<div>Country Performance Alert</div>
+
+</div>
+
+</section>
+
+);
+
+}
