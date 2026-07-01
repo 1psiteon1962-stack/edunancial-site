c1c36e462acceeb5c1e8dfdb34d1622e0e9ee725
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:42:48 -0700

    Create CountryConsumerBehavior component

---
+++ src/components/marketplace/CountryConsumerBehavior.tsx
@@ -0,0 +1,15 @@
+export default function CountryConsumerBehavior(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Consumer Behavior
+
+</h2>
+
+<div className="grid md:grid-cols-3 gap-6 mt-10">
+
+<div>Shopping
