696cffd0896393f0445812e8c302671da87fba29
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:48:48 -0700

    Add Twitter metadata for SEO

---
+++ src/components/seo/TwitterMetadata.tsx
@@ -0,0 +1,13 @@
+export const twitterMetadata={
+
+card:"summary_large_image",
+
+title:"Edunancial",
+
+description:
+
+"Global Financial Literacy & Business Education Platform.",
+
+images:["/images/og-home.jpg"]
+
+};
