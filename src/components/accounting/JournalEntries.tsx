ccda54249657a427c47952002bf9eff72bfebb2d
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 18:32:42 -0700

    Create JournalEntries component for displaying entries

---
+++ src/components/accounting/JournalEntries.tsx
@@ -0,0 +1,31 @@
+export default function JournalEntries(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Journal Entries
+
+</h2>
+
+<div className="grid md:grid-cols-5 gap-6 mt-8">
+
+<div>Date</div>
+
+<div>Reference</div>
+
+<div>Account</div>
+
+<div>Debit</div>
+
+<div>Credit</div>
+
+</div>
+
+</section>
+
+);
+
+}
