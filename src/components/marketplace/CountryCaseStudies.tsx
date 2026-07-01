e19069e121c969c0153743dd99eeaac4c97ce1e1
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:21:52 -0700

    Add CountryCaseStudies component with case studies

---
+++ src/components/marketplace/CountryCaseStudies.tsx
@@ -0,0 +1,56 @@
+export default function CountryCaseStudies(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Country Case Studies
+
+</h2>
+
+<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
+
+<div className="rounded-lg bg-slate-800 p-6">
+
+Uganda
+
+</div>
+
+<div className="rounded-lg bg-slate-800 p-6">
+
+Canada
+
+</div>
+
+<div className="rounded-lg bg-slate-800 p-6">
+
+United States
+
+</div>
+
+<div className="rounded-lg bg-slate-800 p-6">
+
+Dominican Republic
+
+</div>
+
+<div className="rounded-lg bg-slate-800 p-6">
+
+Spain
+
+</div>
+
+<div className="rounded-lg bg-slate-800 p-6">
+
+Kenya
+
+</div>
+
+</div>
+
+</section>
+
+);
+}
