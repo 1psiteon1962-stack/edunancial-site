5ff31f05fc1c4305a23c4b9ad6c75a3d8c2965f8
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:20:34 -0700

    Add CountryPricing component for regional pricing display

---
+++ src/components/marketplace/CountryPricing.tsx
@@ -0,0 +1,57 @@
+export default function CountryPricing(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Regional Pricing
+
+</h2>
+
+<div className="grid md:grid-cols-3 gap-6 mt-10">
+
+<div className="rounded-lg bg-slate-800 p-6">
+
+North America
+
+</div>
+
+<div className="rounded-lg bg-slate-800 p-6">
+
+Europe
+
+</div>
+
+<div className="rounded-lg bg-slate-800 p-6">
+
+Africa
+
+</div>
+
+<div className="rounded-lg bg-slate-800 p-6">
+
+Latin America
+
+</div>
+
+<div className="rounded-lg bg-slate-800 p-6">
+
+Asia
+
+</div>
+
+<div className="rounded-lg bg-slate-800 p-6">
+
+Oceania
+
+</div>
+
+</div>
+
+</section>
+
+);
+
+}
