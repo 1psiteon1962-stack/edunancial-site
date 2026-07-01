766d2aebb2458a7852af9c5ce1b1be98bb08559b
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 17:34:25 -0700

    Add CountryRealEstateProfile component

---
+++ src/components/marketplace/CountryRealEstateProfile.tsx
@@ -0,0 +1,37 @@
+export default function CountryRealEstateProfile(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Real Estate Profile
+
+</h2>
+
+<div className="grid md:grid-cols-2 gap-6 mt-10">
+
+<div>Residential Market</div>
+
+<div>Commercial Market</div>
+
+<div>Financing Options</div>
+
+<div>Owner Financing</div>
+
+<div>Lease Options</div>
+
+<div>Creative Financing</div>
+
+<div>Property Taxes</div>
+
+<div>Closing Costs</div>
+
+</div>
+
+</section>
+
+);
+
+}
