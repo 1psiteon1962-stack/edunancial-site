66b4b219b15633028ddeabb6fa42d8633a42cfa8
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 19:02:11 -0700

    Add MarketplaceRevenueDashboard component

---
+++ src/components/accounting/MarketplaceRevenueDashboard.tsx
@@ -0,0 +1,37 @@
+export default function MarketplaceRevenueDashboard(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Marketplace Revenue
+
+</h2>
+
+<div className="grid md:grid-cols-4 gap-6 mt-8">
+
+<div>Memberships</div>
+
+<div>Professional Fees</div>
+
+<div>Advertising</div>
+
+<div>Featured Listings</div>
+
+<div>Country</div>
+
+<div>Region</div>
+
+<div>Monthly Revenue</div>
+
+<div>Annual Revenue</div>
+
+</div>
+
+</section>
+
+);
+
+}
