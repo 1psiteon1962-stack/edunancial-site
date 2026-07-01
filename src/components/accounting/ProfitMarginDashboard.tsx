ad75f94cce3b5e1efbcb31a98c8fdfb62ccb2f08
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-06-30 18:53:21 -0700

    Create ProfitMarginDashboard component

---
+++ src/components/accounting/ProfitMarginDashboard.tsx
@@ -0,0 +1,37 @@
+export default function ProfitMarginDashboard(){
+
+return(
+
+<section className="rounded-xl bg-slate-900 p-8">
+
+<h2 className="text-4xl font-black">
+
+Profit Margin Analysis
+
+</h2>
+
+<div className="grid md:grid-cols-4 gap-6 mt-10">
+
+<div>Gross Margin</div>
+
+<div>Operating Margin</div>
+
+<div>Net Margin</div>
+
+<div>EBITDA</div>
+
+<div>By Course</div>
+
+<div>By Marketplace</div>
+
+<div>By Country</div>
+
+<div>By Region</div>
+
+</div>
+
+</section>
+
+);
+
+}
