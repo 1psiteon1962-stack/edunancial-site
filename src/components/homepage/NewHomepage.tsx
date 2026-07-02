d636eb2ff3b03313e329463597204e7367e9d30c
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-07-01 19:49:10 -0700

    Add NewHomepage component with subcomponents

---
+++ src/components/homepage/NewHomepage.tsx
@@ -0,0 +1,24 @@
+import NewHero from "./NewHero";
+import CompetencyProgression from "./CompetencyProgression";
+import ChooseYourPath from "./ChooseYourPath";
+import HomepageMission from "./HomepageMission";
+
+export default function NewHomepage(){
+
+return(
+
+<>
+
+<NewHero/>
+
+<CompetencyProgression/>
+
+<ChooseYourPath/>
+
+<HomepageMission/>
+
+</>
+
+);
+
+}
