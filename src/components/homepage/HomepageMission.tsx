1ee2b93cc9385a98b17f998150be08ecc50db881
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-07-01 19:48:17 -0700

    Add HomepageMission component with mission statement

---
+++ src/components/homepage/HomepageMission.tsx
@@ -0,0 +1,23 @@
+export default function HomepageMission(){
+
+return(
+
+<section className="bg-blue-900 py-24 text-center">
+
+<h2 className="text-5xl font-black text-white">
+
+Our Mission
+
+</h2>
+
+<p className="mx-auto mt-10 max-w-5xl text-2xl leading-10 text-blue-100">
+
+Our mission is to help people around the world transform financial literacy into financial competency through education, application, measurement, and continuous improvement.
+
+</p>
+
+</section>
+
+);
+
+}
