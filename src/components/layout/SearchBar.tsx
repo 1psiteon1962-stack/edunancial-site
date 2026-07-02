f3939638021e3b861614d7c316a4da17dde497bd
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-07-01 20:41:30 -0700

    Add SearchBar component for searching topics

---
+++ src/components/layout/SearchBar.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function SearchBar(){
+
+return(
+
+<div className="rounded-xl bg-slate-900 p-6">
+
+<input
+type="search"
+placeholder="Search courses, books, business topics..."
+className="w-full rounded-lg bg-slate-800 p-4 text-white outline-none"
+/>
+
+</div>
+
+);
+
+}
