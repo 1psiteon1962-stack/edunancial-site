d6b5b04c5628ed783e8082b198e4923b06d7b36f
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-07-03 09:01:27 -0700

    Create BusinessAssessmentPage component

---
+++ src/app/business/assessment/page.tsx
@@ -0,0 +1,15 @@
+import BusinessAssessmentForm from "@/components/business/BusinessAssessmentForm";
+
+export default function BusinessAssessmentPage() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#020617",
+        color: "white",
+      }}
+    >
+      <BusinessAssessmentForm />
+    </main>
+  );
+}
