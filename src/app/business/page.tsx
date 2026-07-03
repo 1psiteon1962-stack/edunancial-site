ba698810e9bc36078798bfcb476b242e0f613b54
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-07-03 08:54:44 -0700

    Add BusinessPage component with BusinessTrackSelector

---
+++ src/app/business/page.tsx
@@ -0,0 +1,15 @@
+import BusinessTrackSelector from "@/components/business/BusinessTrackSelector";
+
+export default function BusinessPage() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#020617",
+        color: "white",
+      }}
+    >
+      <BusinessTrackSelector />
+    </main>
+  );
+}
