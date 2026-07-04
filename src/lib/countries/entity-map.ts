f1198b3adf7e8376dd5130fb4f52b692d01d1e30
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-07-04 12:39:31 -0700

    Add OperatingEntity interface and operatingEntities array

---
+++ src/lib/countries/entity-map.ts
@@ -0,0 +1,53 @@
+export interface OperatingEntity {
+
+  entityName: string;
+
+  countries: string[];
+
+}
+
+export const operatingEntities: OperatingEntity[] = [
+
+  {
+
+    entityName: "Edge Financial USA",
+
+    countries: [
+
+      "United States",
+
+      "Canada",
+
+      "Uganda",
+
+      "Nigeria",
+
+    ],
+
+  },
+
+  {
+
+    entityName: "Edge Financial Dominican Republic",
+
+    countries: [
+
+      "Dominican Republic",
+
+    ],
+
+  },
+
+  {
+
+    entityName: "Edge Financial Spain",
+
+    countries: [
+
+      "Spain",
+
+    ],
+
+  },
+
+];
