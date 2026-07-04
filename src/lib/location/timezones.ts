0d966490392098e3cd7fb098b9ccfb582846d2ac
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-07-03 20:08:40 -0700

    Add supported timezones list

---
+++ src/lib/location/timezones.ts
@@ -0,0 +1,23 @@
+export const supportedTimezones = [
+
+  "America/New_York",
+
+  "America/Los_Angeles",
+
+  "America/Phoenix",
+
+  "America/Toronto",
+
+  "Africa/Kampala",
+
+  "Africa/Lagos",
+
+  "Africa/Johannesburg",
+
+  "Africa/Cairo",
+
+  "Africa/Algiers",
+
+  "Africa/Casablanca",
+
+];
