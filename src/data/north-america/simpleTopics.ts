63ef95069a64261a3e926118869e95381d939f35
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-07-08 19:34:39 -0700

    Add North America simple topics data

---
+++ src/data/north-america/simpleTopics.ts
@@ -0,0 +1,12 @@
+export const northAmericaSimpleTopics = [
+  "Budgeting fundamentals",
+  "Credit basics",
+  "Emergency funds",
+  "Debt reduction",
+  "Tax awareness",
+  "Real estate foundations",
+  "Paper asset foundations",
+  "Business foundations",
+  "Risk management",
+  "Financial habits",
+];
