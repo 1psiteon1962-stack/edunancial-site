e3871a71135882f4c771038f2cdec480ceec964e
1psiteon1962-stack <1psiteon1962@gmail.com>
2026-08-27 06:36:27 -0700

    Wire location service to policy-driven pricing

--- src/lib/location/location-service.ts
+++ src/lib/location/location-service.ts
@@ -1,29 +1,27 @@
 import { countries } from "./countries";
 import { regions } from "./regions";
 import { cities } from "./cities";
-import { pricing } from "./pricing";
+import { getCountryPricingPolicy } from "./pricing";
 import { marketplaceProviders } from "./marketplace";
 
 export function getCountry(countryId: string) {
-  return countries.find(c => c.id === countryId);
+  return countries.find((country) => country.id === countryId);
 }
 
 export function getRegion(regionId: string) {
-  return regions.find(r => r.id === regionId);
+  return regions.find((region) => region.id === regionId);
 }
 
 export function getCity(cityId: string) {
-  return cities.find(c => c.id === cityId);
+  return cities.find((city) => city.id === cityId);
 }
 
 export function getPricing(countryId: string) {
-  return pricing.find(p => p.countryId === countryId);
+  return getCountryPricingPolicy(countryId);
 }
 
 export function getMarketplace(countryId: string) {
   return marketplaceProviders.filter(
-    provider =>
-      provider.countryId === countryId &&
-      provider.active
+    (provider) => provider.countryId === countryId && provider.active,
   );
 }
